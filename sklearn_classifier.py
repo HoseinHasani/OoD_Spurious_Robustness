f15b2ca21c42fb85ce4b4ee52e77ac9f88833945
Hosein Hasani <hasanih@ce.sharif.edu>
2023-10-15 23:28:36 +0330

    Visualize logistic regression boundary

--- sklearn_classifier.py
+++ sklearn_classifier.py
@@ -2,9 +2,8 @@ import numpy as np
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
 from sklearn.naive_bayes import GaussianNB
+from matplotlib import pyplot as plt
 from dataset import GaussianDataset
-
-
 
 N_train = 2000
 
@@ -49,7 +48,35 @@ print('Groups Accuracy:', np.round(g_eval_accs, 1))
 print('OoD Probs:', ood_probs)
 print('************')
 
+lim=4
 
+xx, yy = np.mgrid[-lim/2:lim/2:.01, -lim:lim:.01]
+grid = np.c_[xx.ravel(), yy.ravel()]
+probs = clf.predict_proba(grid)[:, 1].reshape(xx.shape)
+
+
+f, ax = plt.subplots(figsize=(8, 6))
+contour = ax.contourf(xx, yy, probs, 25, cmap="RdBu", vmin=0, vmax=1)
+ax_c = f.colorbar(contour)
+ax_c.set_label("$P(y = 1)$")
+ax_c.set_ticks([0, .25, .5, .75, 1])
+
+ax.scatter(x_train[:, 0], x_train[:, 1], c=y_train, s=50,
+           cmap="RdBu", vmin=-.2, vmax=1.2,
+           edgecolor="white", linewidth=1)
+
+ax.scatter(ood_groups[0][:, 0], ood_groups[0][:, 1], c='k', s=50,
+           cmap="RdBu", vmin=-.2, vmax=1.2,
+           edgecolor="white", linewidth=1)
+
+ax.scatter(ood_groups[0][:, 0], ood_groups[1][:, 1], c='k', s=50,
+           cmap="RdBu", vmin=-.2, vmax=1.2,
+           edgecolor="white", linewidth=1)
+
+ax.set(aspect="equal",
+       xlim=(-lim/2, lim/2), ylim=(-lim, lim),
+       xlabel="$X_1$", ylabel="$X_2$")
+    
 clf = GaussianNB()
 clf.fit(x_train, g_train)
 
@@ -62,6 +89,8 @@ print('************')
 print('Total Accuracy:', np.round(eval_acc, 1))
 print('OoD Probs:', ood_probs)
 print('************')
+
+
 
 
 
