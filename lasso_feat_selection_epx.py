9d917e2f77f69630b350360f1c186f66c9fa78d8
Hosein Hasani <hasanih@ce.sharif.edu>
2024-01-18 20:45:23 +0330

    Removing spurious features

--- lasso_feat_selection_epx.py
+++ lasso_feat_selection_epx.py
@@ -49,6 +49,7 @@ mlp = MLP().to(device)
 loss_function = nn.CrossEntropyLoss()
 optimizer = torch.optim.Adam(mlp.parameters(), lr=1e-3)
     
+print('Train core feature classifier:')
 mlp.train()
     
 for e in tqdm.tqdm(range(500)):
@@ -111,6 +112,74 @@ selected_feats = {'blond': blond_feats, 'black': black_feats}
 
 ####################################################
 
+
+mlp_sp = MLP().to(device)  
+loss_function = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(mlp_sp.parameters(), lr=1e-3)
+    
+print('Train spurious feature classifier:')
+mlp_sp.train()
+    
+for e in tqdm.tqdm(range(500)):
+    
+    data_list = []
+    for name in ['woman_black', 'woman_blond']:
+        g_inds = np.random.choice(len(grouped_embs[name]), g_batch_size, replace=False)
+        data_list.append(grouped_embs[name][g_inds])
+    
+    data0 = torch.tensor(np.concatenate(data_list), dtype=torch.float32).to(device)  
+    lbl0 = torch.zeros(2 * g_batch_size, dtype=torch.long).to(device)  
+    
+    data_list = []
+    for name in ['man_black', 'man_blond']:
+        g_inds = np.random.choice(len(grouped_embs[name]), g_batch_size, replace=False)
+        data_list.append(grouped_embs[name][g_inds])
+    
+    data1 = torch.tensor(np.concatenate(data_list), dtype=torch.float32).to(device)  
+    lbl1 = torch.ones(2 * g_batch_size, dtype=torch.long).to(device)  
+    
+    data = torch.cat([data0, data1])
+    lbl = torch.cat([lbl0, lbl1])
+    
+    optimizer.zero_grad()
+    logits = mlp_sp(data)
+    weight = next(mlp_sp.parameters())
+    l1_loss = torch.abs(weight).sum()
+    loss = loss_function(logits, lbl) + gamma * l1_loss
+    loss.backward()
+    optimizer.step()
+    
+    if e % 100 == 0:
+        with torch.no_grad():
+            plt.figure()
+            weight = next(mlp_sp.parameters()).detach().cpu().numpy()
+            plt.hist(np.abs(weight.ravel()), 100)
+            plt.title(str(e))
+            
+            data0 = torch.tensor(np.concatenate([grouped_embs['woman_black'][:1000],
+                                                 grouped_embs['woman_blond'][:1000]]),
+                                                dtype=torch.float32).to(device)
+            lbl0 = torch.zeros(2000, dtype=torch.long).to(device) 
+            
+            data1 = torch.tensor(np.concatenate([grouped_embs['man_black'][:1000],
+                                                 grouped_embs['man_blond'][:1000]]),
+                                                dtype=torch.float32).to(device)
+            lbl1 = torch.ones(2000, dtype=torch.long).to(device) 
+            data = torch.cat([data0, data1])
+            lbl = torch.cat([lbl0, lbl1])
+            pred = mlp_sp(data).max(-1)[1]
+            acc = (pred == lbl).float().mean().item()
+            print('train acc:', acc)
+            
+            
+weight = next(mlp_sp.parameters()).detach().cpu().numpy()
+n_feat = 200
+woman_feats = np.argsort(weight[0])[-n_feat:]
+man_feats = np.argsort(weight[1])[-n_feat:]
+negative_feats = {'woman': woman_feats, 'man': man_feats}
+
+####################################################
+
 def calc_cos_dist2(embs, prototypes, prototype_name):
     if 'blond' in prototype_name:
         feat_inds = selected_feats['blond']
@@ -144,12 +213,19 @@ for group, ax in zip([group for group in grouped_embs if not 'bald' in group], a
 from sklearn.linear_model import LogisticRegression
 
 merged_feats = list(set(np.concatenate([blond_feats, black_feats])))
+merged_negative_feats = list(set(np.concatenate([woman_feats, man_feats])))
+
+print(len(merged_feats), len(merged_negative_feats))
+
+final_feats = [feat for feat in merged_feats if feat not in merged_negative_feats]
+print(len(final_feats))
+
 data0 = np.concatenate([grouped_embs['man_blond'][:1000], grouped_embs['woman_blond'][:1000]])
 lbl0 = np.zeros(2000)
 data1 = np.concatenate([grouped_embs['man_black'][:1000], grouped_embs['woman_black'][:1000]])
 lbl1 = np.ones(2000)
 
-x_train = np.concatenate([data0, data1])[:, merged_feats]
+x_train = np.concatenate([data0, data1])[:, final_feats]
 y_train = np.concatenate([lbl0, lbl1])
 
 clf = LogisticRegression()
@@ -160,31 +236,30 @@ lbl0 = np.zeros(400)
 data1 = np.concatenate([grouped_embs['man_black'][-200:], grouped_embs['woman_black'][-200:]])
 lbl1 = np.ones(400)
 
-x_eval = np.concatenate([data0, data1])[:, merged_feats]
+x_eval = np.concatenate([data0, data1])[:, final_feats]
 y_eval = np.concatenate([lbl0, lbl1])
 
 preds = clf.predict(x_eval)
 eval_acc = 100 * (preds == y_eval).mean()
 print('BLOND / BLACK ACC:', eval_acc)
 
-merged_feats = list(set(np.concatenate([blond_feats, black_feats])))
-data0 = np.concatenate([grouped_embs['man_blond'][:1000], grouped_embs['man_black'][:1000]])
+data0 = np.concatenate([grouped_embs['woman_black'][:1000], grouped_embs['woman_blond'][:1000]])
 lbl0 = np.zeros(2000)
-data1 = np.concatenate([grouped_embs['woman_blond'][:1000], grouped_embs['woman_black'][:1000]])
+data1 = np.concatenate([grouped_embs['man_black'][:1000], grouped_embs['man_blond'][:1000]])
 lbl1 = np.ones(2000)
 
-x_train = np.concatenate([data0, data1])[:, merged_feats]
+x_train = np.concatenate([data0, data1])[:, final_feats]
 y_train = np.concatenate([lbl0, lbl1])
 
 clf = LogisticRegression()
 clf.fit(x_train, y_train)
 
-data0 = np.concatenate([grouped_embs['man_blond'][-200:], grouped_embs['man_black'][-200:]])
+data0 = np.concatenate([grouped_embs['woman_black'][-200:], grouped_embs['woman_blond'][-200:]])
 lbl0 = np.zeros(400)
-data1 = np.concatenate([grouped_embs['woman_blond'][-200:], grouped_embs['woman_black'][-200:]])
+data1 = np.concatenate([grouped_embs['man_black'][-200:], grouped_embs['man_blond'][-200:]])
 lbl1 = np.ones(400)
 
-x_eval = np.concatenate([data0, data1])[:, merged_feats]
+x_eval = np.concatenate([data0, data1])[:, final_feats]
 y_eval = np.concatenate([lbl0, lbl1])
 
 preds = clf.predict(x_eval)
