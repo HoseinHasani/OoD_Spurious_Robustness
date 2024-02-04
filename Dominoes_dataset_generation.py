1c848ee467cb9d008e93522c61c127557c7d7a9d
Hosein Hasani <hasanih@ce.sharif.edu>
2024-02-04 23:58:02 +0330

    Partition cifar and mnist classes for groups

--- Dominoes_dataset_generation.py
+++ Dominoes_dataset_generation.py
@@ -7,12 +7,28 @@ device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 sp_corr = 0.9
 
+n_maj = 1000
+n_min = 500
+
 dataset_path = 'datasets'
 
-train_cifar = CIFAR10(dataset_path, train=True, download=False)
-test_cifar = CIFAR10(dataset_path, train=False, download=False)
+train_cifar = CIFAR10(dataset_path, train=True, download=True)
+test_cifar = CIFAR10(dataset_path, train=False, download=True)
 train_mnist = MNIST(dataset_path, train=True, download=True)
 test_mnist = MNIST(dataset_path, train=False, download=True)
+
+mnist_classes = [0, 1]
+cifar_classes = [0, 1, 8, 9]
+core_classes = cifar_classes[:2]
+ood_classes = cifar_classes[2:]
+
+cifar_class_names = ['airplane', 'car', 'ship', 'truck']
+
+
+cifar_cl_inds = [np.argwhere(np.array(train_cifar.targets) == cl).ravel() for cl in cifar_classes]
+mnist_cl_inds = [np.argwhere(train_mnist.targets.numpy() == cl).ravel() for cl in cifar_classes]
+
+
 
 
 
