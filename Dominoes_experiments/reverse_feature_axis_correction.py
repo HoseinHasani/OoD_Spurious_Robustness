1125719acc83d581e5f801d40a85e6a7b4956c0e
Hosein Hasani <hosein.hasani@sharif.edu>
2024-03-03 22:03:18 +0330

    Analysis on reverse dataset

---
+++ Dominoes_experiments/reverse_feature_axis_correction.py
@@ -0,0 +1,390 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import auc
+import seaborn as sns
+import os
+import warnings
+
+warnings.filterwarnings("ignore")
+sns.set_context("paper", font_scale=1.4)     
+
+seed = 8
+np.random.seed(seed)
+
+samples4prototype = 400
+
+filter_ood = False
+
+core_class_names = ['0', '1']
+ood_class_names = ['2', '3']
+sp_class_names = ['ship', 'truck']
+
+grouped_embs0 = np.load('Dominoes_grouped_embs.npy', allow_pickle=True).item()
+    
+def normalize(x):
+    return x / np.linalg.norm(x, axis=-1, keepdims=True)
+
+def get_prototypes(embeddings, n_data=None):
+    
+    n = len(embeddings)
+    
+    if n_data is None:
+        inds = np.arange(n)
+    else:
+        assert n_data <= n
+        inds = np.random.choice(n, n_data, replace=False)
+    
+    prototype = embeddings[inds].mean(axis=0, keepdims=True)
+    
+    return prototype
+
+
+grouped_embs = {name: normalize(grouped_embs0[name]) for name in grouped_embs0.keys()}
+    
+grouped_prototypes = {group: get_prototypes(embs, samples4prototype)\
+                      for group, embs in grouped_embs.items()}
+
+all_embs = np.concatenate(list(grouped_embs.values()))
+all_prototypes = np.concatenate(list(grouped_prototypes.values()))
+group_names = list(grouped_embs.keys())
+
+
+##############################################################
+
+
+
+#sp_ax1 = normalize(grouped_prototypes[f'1_{core_class_names[0]}'] - grouped_prototypes[f'0_{core_class_names[0]}'])
+#sp_ax2 = normalize(grouped_prototypes[f'1_{core_class_names[1]}'] - grouped_prototypes[f'0_{core_class_names[1]}'])
+#core_ax1 = normalize(grouped_prototypes[f'1_{core_class_names[1]}'] - grouped_prototypes[f'1_{core_class_names[0]}'])
+#core_ax2 = normalize(grouped_prototypes[f'0_{core_class_names[1]}'] - grouped_prototypes[f'0_{core_class_names[0]}'])
+
+
+sp_ax1 = normalize(normalize(grouped_prototypes[f'{core_class_names[0]}_{sp_class_names[0]}'])\
+                   + normalize(grouped_prototypes[f'{core_class_names[1]}_{sp_class_names[0]}']))
+
+sp_ax2 = normalize(normalize(grouped_prototypes[f'{core_class_names[0]}_{sp_class_names[1]}'])\
+                   + normalize(grouped_prototypes[f'{core_class_names[1]}_{sp_class_names[1]}']))
+
+
+core_ax1 = normalize(normalize(grouped_prototypes[f'{core_class_names[0]}_{sp_class_names[0]}'])\
+                   + normalize(grouped_prototypes[f'{core_class_names[0]}_{sp_class_names[1]}']))
+
+core_ax2 = normalize(normalize(grouped_prototypes[f'{core_class_names[1]}_{sp_class_names[0]}'])\
+                   + normalize(grouped_prototypes[f'{core_class_names[1]}_{sp_class_names[1]}']))
+
+
+def refine_embs(embs, sp1, sp2, cr1, cr2):
+    #core = embs * core_ax_normal[None]
+    embs = normalize(embs)
+    #sp_coefs1 = np.dot(embs, sp1.squeeze())
+    #sp_coefs2 = np.dot(embs, sp2.squeeze())
+
+    cr_coefs1 = np.dot(embs, cr1.squeeze())
+    cr_coefs2 = np.dot(embs, cr2.squeeze())
+    
+    #refined = embs.copy()
+    refined = cr_coefs1[:, None] * np.repeat(cr1, embs.shape[0], axis=0)
+    refined += cr_coefs2[:, None] * np.repeat(cr2, embs.shape[0], axis=0)
+
+    sp_coefs1 = np.dot(refined, sp1.squeeze())
+    sp_coefs2 = np.dot(refined, sp2.squeeze())
+    
+    refined -= sp_coefs1[:, None] * np.repeat(sp1, embs.shape[0], axis=0)
+    refined -= sp_coefs2[:, None] * np.repeat(sp2, embs.shape[0], axis=0)
+    
+    if filter_ood:
+        
+        ood_coefs1 = np.dot(refined, ood_ax1.squeeze())
+        ood_coefs2 = np.dot(refined, ood_ax2.squeeze())
+        refined -= ood_coefs1[:, None] * np.repeat(ood_ax1, embs.shape[0], axis=0)
+        refined -= ood_coefs2[:, None] * np.repeat(ood_ax2, embs.shape[0], axis=0)
+        refined += cr_coefs1[:, None] * np.repeat(cr1, embs.shape[0], axis=0)
+        refined += cr_coefs2[:, None] * np.repeat(cr2, embs.shape[0], axis=0)
+                
+    
+    refined = normalize(refined)
+    return refined
+
+
+refined_grouped_embs = {}
+for key in grouped_embs.keys():
+    refined_grouped_embs[key] = refine_embs(grouped_embs[key], sp_ax1, sp_ax2, core_ax1, core_ax2)
+
+corrupted_grouped_embs = {}
+for key in grouped_embs.keys():
+    corrupted_grouped_embs[key] = refine_embs(grouped_embs[key], core_ax1, core_ax2, sp_ax1, sp_ax2)
+
+
+refined_grouped_prototypes = {group: get_prototypes(embs, samples4prototype)\
+                              for group, embs in refined_grouped_embs.items()}
+
+
+##############################################################
+
+def prepare_class_data(embs_dict, group_names, len_g, lbl_val, inds=None):
+    data_list = []
+    for name in group_names:
+        if inds is None:
+            g_inds = np.random.choice(len(embs_dict[name]), len_g, replace=False)
+        else:
+            g_inds = inds
+            
+        assert len(g_inds) == len_g
+        
+        data_list.append(embs_dict[name][g_inds])
+    
+    data = np.concatenate(data_list)
+    labels = lbl_val * np.ones(len(group_names) * len_g)
+    
+    return data, labels
+
+def prepare_data(embs_dict, names_0, names_1, len_g, inds=None):
+    data0, lbl0 = prepare_class_data(embs_dict, names_0, len_g, 0, inds)
+    data1, lbl1 = prepare_class_data(embs_dict, names_1, len_g, 1, inds)
+    
+    data_np = np.concatenate([data0, data1])
+    lbl_np = np.concatenate([lbl0, lbl1]).astype(int)
+    
+    return data_np, lbl_np
+
+
+# ##############################################################
+
+# print('Neutral version:')
+
+# x_train, y_train = prepare_data(grouped_embs, [f'0_{core_class_names[0]}', f'0_{core_class_names[1]}'], [f'1_{core_class_names[0]}', f'1_{core_class_names[1]}'], 300, np.arange(300))
+# clf = LogisticRegression()
+# clf.fit(x_train, y_train)
+
+# x_eval, y_eval = prepare_data(grouped_embs, [f'0_{core_class_names[0]}', f'0_{core_class_names[1]}'], [f'1_{core_class_names[0]}', f'1_{core_class_names[1]}'], 300, np.arange(-300,0))
+# preds = clf.predict(x_eval)
+# eval_acc = 100 * (preds == y_eval).mean()
+
+# print('ZERO / ONE ACC:', eval_acc)
+
+# x_train, y_train = prepare_data(grouped_embs, [f'0_{core_class_names[0]}', f'1_{core_class_names[0]}'], [f'0_{core_class_names[1]}', f'1_{core_class_names[1]}'], 300, np.arange(300))
+# clf = LogisticRegression()
+# clf.fit(x_train, y_train)
+
+# x_eval, y_eval = prepare_data(grouped_embs, [f'0_{core_class_names[0]}', f'1_{core_class_names[0]}'], [f'0_{core_class_names[1]}', f'1_{core_class_names[1]}'], 300, np.arange(-300,0))
+# preds = clf.predict(x_eval)
+# eval_acc = 100 * (preds == y_eval).mean()
+
+# print('{core_class_names[0]} / {core_class_names[1]} ACC:', eval_acc)
+
+
+# ##############################################################
+
+# print('Refined version:')
+
+# x_train, y_train = prepare_data(grouped_embs, [f'0_{core_class_names[0]}', f'0_{core_class_names[1]}'], [f'1_{core_class_names[0]}', f'1_{core_class_names[1]}'], 300, np.arange(300))
+# clf = LogisticRegression()
+# clf.fit(x_train, y_train)
+
+# x_eval, y_eval = prepare_data(refined_grouped_embs, [f'0_{core_class_names[0]}', f'0_{core_class_names[1]}'], [f'1_{core_class_names[0]}', f'1_{core_class_names[1]}'], 300, np.arange(-300,0))
+# preds = clf.predict(x_eval)
+# eval_acc = 100 * (preds == y_eval).mean()
+
+# print('ZERO / ONE ACC:', eval_acc)
+
+# x_train, y_train = prepare_data(grouped_embs, [f'0_{core_class_names[0]}', f'1_{core_class_names[0]}'], [f'0_{core_class_names[1]}', f'1_{core_class_names[1]}'], 300, np.arange(300))
+# clf = LogisticRegression()
+# clf.fit(x_train, y_train)
+
+# x_eval, y_eval = prepare_data(refined_grouped_embs, [f'0_{core_class_names[0]}', f'1_{core_class_names[0]}'], [f'0_{core_class_names[1]}', f'1_{core_class_names[1]}'], 300, np.arange(-300,0))
+# preds = clf.predict(x_eval)
+# eval_acc = 100 * (preds == y_eval).mean()
+
+# print('{core_class_names[0]} / {core_class_names[1]} ACC:', eval_acc)
+
+# ##############################################################
+# print('Corrupted version:')
+
+# x_train, y_train = prepare_data(grouped_embs, [f'0_{core_class_names[0]}', f'0_{core_class_names[1]}'], [f'1_{core_class_names[0]}', f'1_{core_class_names[1]}'], 300, np.arange(300))
+# clf = LogisticRegression()
+# clf.fit(x_train, y_train)
+
+# x_eval, y_eval = prepare_data(corrupted_grouped_embs, [f'0_{core_class_names[0]}', f'0_{core_class_names[1]}'], [f'1_{core_class_names[0]}', f'1_{core_class_names[1]}'], 300, np.arange(-300,0))
+# preds = clf.predict(x_eval)
+# eval_acc = 100 * (preds == y_eval).mean()
+
+# print('ZERO / ONE ACC:', eval_acc)
+
+# x_train, y_train = prepare_data(grouped_embs, [f'0_{core_class_names[0]}', f'1_{core_class_names[0]}'], [f'0_{core_class_names[1]}', f'1_{core_class_names[1]}'], 300, np.arange(300))
+# clf = LogisticRegression()
+# clf.fit(x_train, y_train)
+
+# x_eval, y_eval = prepare_data(corrupted_grouped_embs, [f'0_{core_class_names[0]}', f'1_{core_class_names[0]}'], [f'0_{core_class_names[1]}', f'1_{core_class_names[1]}'], 300, np.arange(-300,0))
+# preds = clf.predict(x_eval)
+# eval_acc = 100 * (preds == y_eval).mean()
+
+# print('{core_class_names[0]} / {core_class_names[1]} ACC:', eval_acc)
+
+# ##############################################################
+print()
+
+
+def calc_cos_dist(embs, prototypes):
+    embs_normalized = embs / np.linalg.norm(embs, axis=-1, keepdims=True)
+    prototypes_normalized = prototypes / np.linalg.norm(prototypes, axis=-1, keepdims=True)
+    cos_dist = (1 - (embs_normalized[:, None] * prototypes_normalized).sum(axis=-1)) / 2
+    dist = np.abs(cos_dist.squeeze())
+    return dist
+
+
+grouped_cos_dist = {group: calc_cos_dist(embs, refined_grouped_prototypes[group])\
+                    for group, embs in refined_grouped_embs.items()}
+
+selected_groups_names = []
+for i in range(2):
+  for j in range(2):
+    selected_group_names = [f'{core_class_names[i]}_{sp_class_names[j]}']
+selected_grouped_embs = {name: refined_grouped_embs[name] for name in selected_groups_names}
+    
+fig, axes = plt.subplots(2, 2, figsize=(10, 10))
+axes = axes.flatten()
+for group, ax in zip([group for group in selected_grouped_embs], axes):
+    sns.histplot(grouped_cos_dist[group], label=group, palette=['red'], ax=ax, element='step', linewidth=2.5, fill=False)
+    loc = group.find('_')
+    sp_name = group[loc + 1:]
+    ood_embs = np.concatenate([refined_grouped_embs[class_name + '_' + sp_name] for class_name in ood_class_names])
+    sns.histplot(calc_cos_dist(ood_embs, refined_grouped_prototypes[group]), label='ood', ax=ax, element='step', linewidth=2.5, fill=False)
+    ax.legend()
+    ax.set_title(group, fontsize=17)
+    
+    
+grouped_cos_dist = {group: calc_cos_dist(embs, grouped_prototypes[group]) for group, embs in grouped_embs.items()}
+
+selected_grouped_embs = {name: grouped_embs[name] for name in selected_groups_names}
+    
+fig, axes = plt.subplots(2, 2, figsize=(10, 10))
+axes = axes.flatten()
+for group, ax in zip([group for group in selected_grouped_embs], axes):
+    sns.histplot(grouped_cos_dist[group], label=group, palette=['red'], ax=ax, element='step', linewidth=2.5, fill=False)
+    loc = group.find('_')
+    sp_name = group[loc + 1:]
+    ood_embs = np.concatenate([grouped_embs[class_name + '_' + sp_name] for class_name in ood_class_names])
+    sns.histplot(calc_cos_dist(ood_embs, grouped_prototypes[group]), label=f'ood', ax=ax, element='step', linewidth=2.5, fill=False)
+    ax.legend()
+    ax.set_title(group, fontsize=17)
+    
+def get_dist_vals(emb_name1, emb_name2, pr_name1, pr_name2, refined=False):
+    
+    if refined:
+        embs = refined_grouped_embs
+        protos = refined_grouped_prototypes
+    else:
+        embs = grouped_embs
+        protos = grouped_prototypes
+        
+    dist_vals = calc_cos_dist(embs[emb_name1 + emb_name2],
+                              protos[pr_name1 + pr_name2])
+    return dist_vals
+    
+
+def find_thresh_val(main_vals, th=0.95):
+    thresh = np.sort(main_vals)[int(th * len(main_vals))]
+    return thresh
+    
+
+    
+for ood_name in ood_class_names:
+    for core_name in core_class_names:
+        print(f'core name: {core_name}, ood name: {ood_name}')
+
+        neutral_ood = np.concatenate([
+                get_dist_vals('0_', ood_name, '0_', core_name),
+                get_dist_vals('1_', ood_name, '1_', core_name)
+                ])
+
+        refined_ood = np.concatenate([
+                get_dist_vals('0_', ood_name, '0_', core_name, refined=True),
+                get_dist_vals('1_', ood_name, '1_', core_name, refined=True)
+                ])
+    
+        neutral_ind = np.concatenate([
+                get_dist_vals('0_', core_name, '0_', core_name),
+                get_dist_vals('1_', core_name, '1_', core_name)
+                ])
+
+        refined_ind = np.concatenate([
+                get_dist_vals('0_', core_name, '0_', core_name, refined=True),
+                get_dist_vals('1_', core_name, '1_', core_name, refined=True)
+                ])
+    
+        neutral_sp = np.concatenate([
+                get_dist_vals('0_', core_name, '1_', core_name),
+                get_dist_vals('1_', core_name, '0_', core_name)
+                ])
+
+        refined_sp = np.concatenate([
+                get_dist_vals('0_', core_name, '1_', core_name, refined=True),
+                get_dist_vals('1_', core_name, '0_', core_name, refined=True)
+                ])
+
+    
+        neutral_main = np.concatenate([neutral_ind, neutral_sp])
+                
+        neutral_th = find_thresh_val(neutral_main)
+        neutral_err = neutral_ood[neutral_ood < neutral_th].shape[0] / neutral_ood.shape[0]
+
+        refined_main = np.concatenate([refined_ind, refined_sp])
+        
+        refined_th = find_thresh_val(refined_main)
+        refined_err = refined_ood[refined_ood < refined_th].shape[0] / refined_ood.shape[0]
+        
+        print('neutral:', 100 * neutral_err,
+              np.mean(neutral_ind) / np.mean(neutral_ood),
+              np.mean(neutral_sp) / np.mean(neutral_ood))
+        
+        print('refined:', 100 * refined_err,
+              np.mean(np.mean(refined_ind)) / np.mean(np.mean(refined_ood)),
+              np.mean(refined_sp) / np.mean(refined_ood))
+        
+        print('***********************')
+        
+        
+        thresholds = [th for th in np.arange(1, 100) / 100]
+        
+        n_fps = [0]
+        n_tps = [0]
+        
+        r_fps = [0]
+        r_tps = [0]
+        
+        for th in thresholds:
+            
+            neutral_th = find_thresh_val(np.concatenate([neutral_ind, neutral_sp]), th)
+            
+            neutral_fp = neutral_ood[neutral_ood < neutral_th].shape[0] / neutral_ood.shape[0]
+            neutral_tp = neutral_main[neutral_main < neutral_th].shape[0] / neutral_main.shape[0]
+            
+            n_fps.append(neutral_fp)
+            n_tps.append(neutral_tp)
+
+            refined_th = find_thresh_val(np.concatenate([refined_ind, refined_sp]), th)
+            
+            refined_fp = refined_ood[refined_ood < refined_th].shape[0] / refined_ood.shape[0]
+            refined_tp = refined_main[refined_main < refined_th].shape[0] / refined_main.shape[0]
+            
+            r_fps.append(refined_fp)
+            r_tps.append(refined_tp)
+                    
+        n_fps.append(1)
+        n_tps.append(1)
+        r_fps.append(1)
+        r_tps.append(1)
+        
+        n_auc = np.round(auc(n_fps, n_tps), 3)
+        r_auc = np.round(auc(r_fps, r_tps), 3)
+        
+        plt.figure()
+        plt.plot(n_fps, n_tps, label=f'before refinement, area={n_auc}', linewidth=2)
+        plt.plot(r_fps, r_tps, label=f'after refinement, area={r_auc}', linewidth=2)
+        plt.xlabel('FPR')
+        plt.ylabel('TPR')
+        plt.ylim([0.55, 1.001])
+        plt.legend()
+        plt.title(f'ROC ({core_name})', fontsize=17)
+        
