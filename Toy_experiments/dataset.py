ba585184a2d41435cefa4585c3953efaa8bc437d
Hosein Hasani <hasanih@ce.sharif.edu>
2024-03-20 19:16:18 +0330

    Working on 2D toy dataset

--- Toy_experiments/dataset.py
+++ Toy_experiments/dataset.py
@@ -41,6 +41,85 @@ class GaussianDataset():
         self.o = [ood0, ood1]
         
         
+class GaussianDataset2D():
+    def __init__(self, seed=None, maj_size=5000, min_size=3000, std=0.35, normal=True):
+        
+        self.std_maj = std
+        self.std_min = std * 0.7
+        self.maj_size = maj_size
+        self.min_size = min_size
+        self.grouped_embs = {}
+        self.normal = normal
+        
+        if seed is not None:
+            self.set_seed(seed)
+        
+        self.core_ax, self.sp_ax = self.generate_random_axes()
+        self.generate_dataset()
+
+    def set_seed(self, seed):
+        np.random.seed(seed)
+        
+    def normalize(self, x, axis=False):
+        
+        normal = self.normal or axis
+        
+        if normal:
+            return x / np.linalg.norm(x, axis=-1, keepdims=True)
+        else:
+            return x
+
+    def generate_random_axes(self):
+#        core_axis = self.normalize(2 * np.random.rand(2) - 1, True)
+#        sp_axis = self.normalize(2 * np.random.rand(2) - 1, True)
+        core_axis = np.array([1, 0])
+        sp_axis = np.array([0, 1])
+        return core_axis, sp_axis
+    
+    def generate_dataset(self, alpha=0.6):
+        
+        mean = -alpha * self.sp_ax + 1 * self.core_ax
+        min0 = np.random.normal(mean, self.std_min, size=(self.min_size, 2))
+        min0 = self.normalize(min0)
+        
+        mean = (1 - alpha) * self.sp_ax + 1 * self.core_ax
+        maj0 = np.random.normal(mean, self.std_maj, size=(self.maj_size, 2))
+        maj0 = self.normalize(maj0)
+
+        mean = alpha * self.sp_ax - 1 * self.core_ax
+        min1 = np.random.normal(mean, self.std_min, size=(self.min_size, 2))
+        min1 = self.normalize(min1)
+        
+        mean = -(1 - alpha) * self.sp_ax - 1 * self.core_ax
+        maj1 = np.random.normal(mean, self.std_maj, size=(self.maj_size, 2))
+        maj1 = self.normalize(maj1)
+        
+        ood0 = np.random.normal(self.sp_ax, self.std_maj, size=(self.maj_size, 2))
+        
+        cr_coefs = np.dot(ood0, self.core_ax)
+        ood0 -= 0.7 * cr_coefs[:, None] * np.repeat(self.core_ax[None], ood0.shape[0], axis=0)
+        ood0 = self.normalize(ood0)
+        
+        
+        self.grouped_embs['0_0'] = maj0
+        self.grouped_embs['0_1'] = min0
+
+        self.grouped_embs['1_0'] = min1
+        self.grouped_embs['1_1'] = maj1
+        
+        data = np.concatenate([min0, maj0, min1, maj1], 0)
+        labels = np.concatenate([np.zeros(self.min_size + self.maj_size), np.ones(self.min_size + self.maj_size)])
+        group_labels = np.concatenate([0 * np.ones(self.min_size), 1 * np.ones(self.maj_size), 2 * np.ones(self.min_size), 3 * np.ones(self.maj_size)])
+        
+        permutation = np.random.permutation(2 * (self.min_size + self.maj_size))
+
+        self.x = data[permutation]
+        self.y = labels[permutation]
+        self.g = group_labels[permutation]
+        self.o = [ood0]
+        
+        
+        
 class GaussianDataset3D():
     def __init__(self, seed=None, maj_size=3000, min_size=1000, std=0.35, normal=True):
         
---
+++ Toy_experiments/refinement_experiments_2D.py
@@ -0,0 +1,275 @@
+import numpy as np
+from matplotlib import pyplot as plt
+from dataset import GaussianDataset2D
+from scipy.stats import wasserstein_distance
+import seaborn as sns
+from sklearn.metrics import auc
+
+#%matplotlib qt
+
+dataset = GaussianDataset2D(3, normal=False)
+g_embs = dataset.grouped_embs
+ood_embs = dataset.o[0]
+
+core_ax_th = 0.4
+sp_ax_th = 0.8
+
+print(np.dot(dataset.sp_ax, dataset.core_ax))
+
+core_class_names = ['0', '1']
+sp_class_names = ['0', '1']
+
+def draw_point_cloud3D(ax, data, label, color, alpha=0.15):
+    ax.scatter(data[:, 0], data[:, 1], color=color, label=label, alpha=alpha, s=2)
+
+def plot_prototype3D(ax, data, label, color):
+    ax.scatter(data[0], data[1], color=color, label=label, alpha=1, s=12)
+    
+def draw_arrow3D(ax, arrow, label, color, linestyle):
+    ax.quiver(0, 0, arrow[0], arrow[1],
+              label=label, color=color, linestyle=linestyle, linewidth=2)
+    
+    
+def normalize(x):
+    return x / np.linalg.norm(x, axis=-1, keepdims=True)
+    
+
+def refine_embs(embs, sp1, sp2, cr1, cr2):
+    
+    refined = 1.0 * embs.copy()
+
+#    cr_coefs1 = np.dot(dataset.sp_ax, dataset.core_ax)
+#    core_ax = dataset.core_ax - cr_coefs1 * dataset.sp_ax
+#    cr1 = normalize(core_ax)[None]
+    
+    cr_coefs1 = np.dot(embs, cr1.squeeze())
+    cr_coefs1 = np.clip(cr_coefs1, -core_ax_th, core_ax_th)
+    refined += 4 * cr_coefs1[:, None] * np.repeat(cr1, embs.shape[0], axis=0)
+    
+    
+
+#    cr_coefs1 = np.dot(dataset.sp_ax, dataset.core_ax)
+#    sp_ax = dataset.sp_ax - cr_coefs1 * dataset.core_ax
+#    sp1 = normalize(sp_ax)[None]
+    
+    sp_coefs1 = np.dot(refined, sp1.squeeze())
+    sp_coefs1 = np.clip(sp_coefs1, -sp_ax_th, sp_ax_th)
+    refined -= 0.2 * sp_coefs1[:, None] * np.repeat(sp1, embs.shape[0], axis=0)
+
+    
+#    refined = normalize(refined)
+    return refined
+   
+    
+   
+figsize = 10
+fig = plt.figure(figsize=(figsize, figsize))
+
+ax = fig.add_subplot()
+
+draw_point_cloud3D(ax, g_embs['0_0'], 'maj 0', 'tab:blue')
+draw_point_cloud3D(ax, g_embs['0_1'], 'min 0', 'tab:green')
+draw_point_cloud3D(ax, g_embs['1_0'], 'maj 1', 'tab:orange')
+draw_point_cloud3D(ax, g_embs['1_1'], 'min 1', 'tab:red')
+
+draw_point_cloud3D(ax, ood_embs, 'OoD', 'tab:gray')
+
+plot_prototype3D(ax, g_embs['0_0'].mean(0), 'maj 0 - prototype', 'blue')
+plot_prototype3D(ax, g_embs['0_1'].mean(0), 'maj 0 - prototype', 'green')
+plot_prototype3D(ax, g_embs['1_0'].mean(0), 'maj 1 - prototype', 'orange')
+plot_prototype3D(ax, g_embs['1_1'].mean(0), 'maj 1 - prototype', 'red')
+
+draw_arrow3D(ax, dataset.core_ax, 'core axis', 'red', 'solid')
+draw_arrow3D(ax, dataset.sp_ax, 'spurious axis', 'orange', 'dashed')
+#draw_arrow3D(ax, dataset.perp_ax, 'perp axis', 'gray', 'dashed')
+
+
+fig.tight_layout()
+plt.legend()
+
+
+
+refined_g_embs = {}
+for key in g_embs.keys():
+    refined_g_embs[key] = refine_embs(g_embs[key], dataset.sp_ax[None], dataset.sp_ax[None], dataset.core_ax[None], dataset.core_ax[None])
+
+refined_ood_embs = refine_embs(ood_embs, dataset.sp_ax[None], dataset.sp_ax[None], dataset.core_ax[None], dataset.core_ax[None])
+
+
+fig = plt.figure(figsize=(figsize, figsize))
+
+ax = fig.add_subplot()
+
+draw_point_cloud3D(ax, refined_g_embs['0_0'], 'maj 0', 'tab:blue')
+draw_point_cloud3D(ax, refined_g_embs['0_1'], 'min 0', 'tab:green')
+draw_point_cloud3D(ax, refined_g_embs['1_0'], 'maj 1', 'tab:orange')
+draw_point_cloud3D(ax, refined_g_embs['1_1'], 'min 1', 'tab:red')
+
+draw_point_cloud3D(ax, refined_ood_embs, 'OoD', 'tab:gray')
+
+plot_prototype3D(ax, refined_g_embs['0_0'].mean(0), 'maj 0 - prototype', 'blue')
+plot_prototype3D(ax, refined_g_embs['0_1'].mean(0), 'maj 0 - prototype', 'green')
+plot_prototype3D(ax, refined_g_embs['1_0'].mean(0), 'maj 1 - prototype', 'orange')
+plot_prototype3D(ax, refined_g_embs['1_1'].mean(0), 'maj 1 - prototype', 'red')
+
+draw_arrow3D(ax, dataset.core_ax, 'core axis', 'red', 'solid')
+draw_arrow3D(ax, dataset.sp_ax, 'spurious axis', 'orange', 'dashed')
+
+
+fig.tight_layout()
+plt.legend()
+
+#def calc_cos_dist(embs, prototypes):
+##    embs = embs / np.linalg.norm(embs, axis=-1, keepdims=True)
+##    prototypes = prototypes / np.linalg.norm(prototypes, axis=-1, keepdims=True)
+#    cos_dist = (1 - (embs * prototypes).sum(axis=-1)) / 2
+#    #cos_dist = np.abs(cos_dist)
+#    return cos_dist.squeeze()
+
+def calc_euc_dist(embs, prototypes):
+    euc_dist = np.linalg.norm(embs - prototypes, axis=-1)
+    return euc_dist
+
+grouped_cos_dist = {group: calc_euc_dist(embs, g_embs[group].mean(0)) for group, embs in g_embs.items()}
+
+
+print('neutral:')
+fig, axes = plt.subplots(2, 2, figsize=(10, 10))
+axes = axes.flatten()
+for group, ax in zip([group for group in g_embs], axes):
+    sns.histplot(grouped_cos_dist[group], label=group, palette=['red'], ax=ax, element='step', linewidth=2.5, fill=False)
+    loc = group.find('_')
+    sp_name = group[loc + 1:]
+    ood_embs_arr = ood_embs
+    ood_dists = calc_euc_dist(ood_embs_arr, g_embs[group].mean(0))
+    sns.histplot(ood_dists, label='ood', ax=ax, element='step', linewidth=2.5, fill=False)
+    ax.legend()
+    ax.set_title(group, fontsize=17)
+    print(group, sp_name, np.mean(ood_dists) / np.mean(grouped_cos_dist[group]),
+          wasserstein_distance(ood_dists, grouped_cos_dist[group]))
+    
+    
+
+grouped_cos_dist = {group: calc_euc_dist(embs, refined_g_embs[group].mean(0)) for group, embs in refined_g_embs.items()}
+
+
+print('refined:')
+fig, axes = plt.subplots(2, 2, figsize=(10, 10))
+axes = axes.flatten()
+for group, ax in zip([group for group in refined_g_embs], axes):
+    sns.histplot(grouped_cos_dist[group], label=group, palette=['red'], ax=ax, element='step', linewidth=2.5, fill=False)
+    loc = group.find('_')
+    sp_name = group[loc + 1:]
+    ood_embs_arr = refined_ood_embs
+    ood_dists = calc_euc_dist(ood_embs_arr, refined_g_embs[group].mean(0))
+    sns.histplot(ood_dists, label='ood', ax=ax, element='step', linewidth=2.5, fill=False)
+    ax.legend()
+    ax.set_title(group, fontsize=17)
+    print(group, sp_name, np.mean(ood_dists) / np.mean(grouped_cos_dist[group]),
+          wasserstein_distance(ood_dists, grouped_cos_dist[group]))
+    
+    
+print('***********************')
+    
+    
+    
+def get_dist_vals(emb_name1, emb_name2, pr_name1, pr_name2, refined=False):
+    
+    if refined:
+        embs = refined_g_embs
+    else:
+        embs = g_embs
+        
+    dist_vals = calc_euc_dist(embs[emb_name1 + '_' + emb_name2],
+                              embs[pr_name1 + '_' + pr_name2].mean(0))
+    return dist_vals
+    
+
+def get_dist_vals_ood(pr_name1, pr_name2, refined=False):
+    
+    if refined:
+        embs = refined_ood_embs
+        p_embs = refined_g_embs
+    else:
+        embs = ood_embs
+        p_embs = g_embs
+        
+    dist_vals = calc_euc_dist(embs,
+                              p_embs[pr_name1 + '_' + pr_name2].mean(0))
+    return dist_vals
+
+
+def find_thresh_val(main_vals, th=0.95):
+    thresh = np.sort(main_vals)[int(th * len(main_vals))]
+    return thresh
+    
+
+    
+for core_name in core_class_names:
+    for sp_name in sp_class_names:
+        neutral_ood = get_dist_vals_ood(core_name, sp_name)
+        refined_ood = get_dist_vals_ood(core_name, sp_name, refined=True)
+    
+        neutral_ind = get_dist_vals(core_name, sp_name, core_name, sp_name)
+        refined_ind = get_dist_vals(core_name, sp_name, core_name, sp_name, refined=True)
+    
+                
+        neutral_th = find_thresh_val(neutral_ind)
+        neutral_err = neutral_ood[neutral_ood < neutral_th].shape[0] / neutral_ood.shape[0]
+    
+        
+        refined_th = find_thresh_val(refined_ind)
+        refined_err = refined_ood[refined_ood < refined_th].shape[0] / refined_ood.shape[0]
+        
+        print('neutral:', 100 * neutral_err,
+              np.mean(neutral_ind) / np.mean(neutral_ood))
+        
+        print('refined:', 100 * refined_err,
+              np.mean(np.mean(refined_ind)) / np.mean(np.mean(refined_ood)))
+        
+        print('***********************')
+        
+        
+        thresholds = [th for th in np.arange(1, 100) / 100]
+        
+        n_fps = [0]
+        n_tps = [0]
+        
+        r_fps = [0]
+        r_tps = [0]
+        
+        for th in thresholds:
+            
+            neutral_th = find_thresh_val(neutral_ind, th)
+            
+            neutral_fp = neutral_ood[neutral_ood < neutral_th].shape[0] / neutral_ood.shape[0]
+            neutral_tp = neutral_ind[neutral_ind < neutral_th].shape[0] / neutral_ind.shape[0]
+            
+            n_fps.append(neutral_fp)
+            n_tps.append(neutral_tp)
+    
+            refined_th = find_thresh_val(refined_ind, th)
+            
+            refined_fp = refined_ood[refined_ood < refined_th].shape[0] / refined_ood.shape[0]
+            refined_tp = refined_ind[refined_ind < refined_th].shape[0] / refined_ind.shape[0]
+            
+            r_fps.append(refined_fp)
+            r_tps.append(refined_tp)
+                    
+        n_fps.append(1)
+        n_tps.append(1)
+        r_fps.append(1)
+        r_tps.append(1)
+        
+        n_auc = np.round(auc(n_fps, n_tps), 4)
+        r_auc = np.round(auc(r_fps, r_tps), 4)
+        
+        plt.figure()
+        plt.plot(n_fps, n_tps, label=f'before refinement, area={n_auc}', linewidth=2)
+        plt.plot(r_fps, r_tps, label=f'after refinement, area={r_auc}', linewidth=2)
+        plt.xlabel('FPR')
+        plt.ylabel('TPR')
+        #plt.ylim([0.55, 1.001])
+        plt.legend()
+        plt.title(f'ROC ({core_name}_{sp_name})', fontsize=17)
+    
