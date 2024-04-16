30ff1da50bd51c0c6295ccd911132cf6759ba84d
Hosein Hasani <hasanih@ce.sharif.edu>
2024-04-16 21:56:42 +0330

    Waterbird prototypical

---
+++ Waterbird_experiments/mlp_experiment/prototypical_learning.py
@@ -0,0 +1,319 @@
+import torch
+from torch import nn
+import torch.nn.functional as F
+import numpy as np
+import matplotlib.pyplot as plt
+import nn_utils
+import os
+import tqdm
+import warnings
+warnings.filterwarnings("ignore")
+
+normalize_embs = True
+
+n_steps = 100
+n_feats = 1024
+sp_rate = 0.5
+
+n_way = 2
+n_support = 10
+n_query = 10
+n_query_test = 100
+
+
+backbones = ['dino', 'res50', 'res18']
+backbone = backbones[0]
+resnet_types = ['pretrained', 'finetuned', 'scratch']
+resnet_type = resnet_types[0]
+
+core_class_names = ['0', '1']
+ood_class_names = ['0', '1']
+sp_class_names = ['0', '1']
+place_names = ['land', 'water']
+
+data_path = '../embeddings/'
+
+
+
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+
+
+if backbone == 'dino':
+    in_data_embs0 = np.load(data_path + 'waterbird_embs.npy', allow_pickle=True).item()
+elif backbone == 'res50':
+    in_data_embs0 = np.load(data_path + f'wb_embs_{backbone}_{resnet_type}.npy', allow_pickle=True).item()
+
+ood_embs0 = {}
+if backbone == 'dino':
+    dict_ = np.load(data_path + 'land.npy', allow_pickle=True).item()
+elif backbone == 'res50':
+    dict_ = np.load(data_path + f'land_{backbone}_{resnet_type}.npy', allow_pickle=True).item()
+    
+ood_embs0['0'] = np.array([dict_[key].squeeze() for key in dict_.keys()])
+
+if backbone == 'dino':
+    dict_ = np.load(data_path + 'water.npy', allow_pickle=True).item()
+elif backbone == 'res50':
+    dict_ = np.load(data_path + f'water_{backbone}_{resnet_type}.npy', allow_pickle=True).item()
+ood_embs0['1'] = np.array([dict_[key].squeeze() for key in dict_.keys()])
+
+grouped_embs0 = {}
+grouped_embs_train0 = {}
+
+for key in in_data_embs0.keys():
+    emb = in_data_embs0[key].squeeze()
+    label = key[0]
+    place = key[2]
+    split = key[4]
+    name = f'{label}_{place}'
+    
+    if split != '0':
+        if name not in grouped_embs0.keys():
+            grouped_embs0[name] = []
+        
+        grouped_embs0[name].append(emb)
+    else:
+        if name not in grouped_embs_train0.keys():
+            grouped_embs_train0[name] = []
+        
+        grouped_embs_train0[name].append(emb)
+
+grouped_embs0 = {name: np.array(grouped_embs0[name]) for name in grouped_embs0.keys()}
+grouped_embs_train0 = {name: np.array(grouped_embs_train0[name]) for name in grouped_embs_train0.keys()}
+
+
+
+def normalize(x):
+    return x / np.linalg.norm(x, axis=-1, keepdims=True)
+
+
+grouped_embs = {name: grouped_embs0[name] for name in grouped_embs0.keys()}
+grouped_embs_train = {name: grouped_embs_train0[name] for name in grouped_embs_train0.keys()}
+ood_embs = {name: ood_embs0[name] for name in ood_embs0.keys()}
+
+
+if normalize_embs:
+    train_dict = {key: normalize(grouped_embs_train0[key]) for key in grouped_embs_train0.keys()}
+    test_dict = {key: normalize(grouped_embs0[key]) for key in grouped_embs0.keys()}
+    ood_dict = {key: normalize(ood_embs0[key]) for key in ood_embs0.keys()}
+else:
+    train_dict = grouped_embs_train0
+    test_dict = grouped_embs
+    ood_dict = ood_embs0
+
+
+
+l_maj0 = len(train_dict[f'{core_class_names[0]}_{sp_class_names[0]}'])
+l_min0 = len(train_dict[f'{core_class_names[0]}_{sp_class_names[1]}'])
+
+l_maj1 = len(train_dict[f'{core_class_names[1]}_{sp_class_names[1]}'])
+l_min1 = len(train_dict[f'{core_class_names[1]}_{sp_class_names[0]}'])
+
+
+def sample_data(n_data, sp_rate):
+    
+    n_maj = int(sp_rate * n_data)
+    n_min = n_data - n_maj
+    
+    ind_0_maj = np.random.choice(l_maj0, size=n_maj, replace=False).ravel()
+    ind_0_min = np.random.choice(l_min0, size=n_min, replace=False).ravel()
+
+    ind_1_maj = np.random.choice(l_maj1, size=n_maj, replace=False).ravel()
+    ind_1_min = np.random.choice(l_min1, size=n_min, replace=False).ravel()
+        
+    data0 = np.concatenate([train_dict[f'{core_class_names[0]}_{sp_class_names[0]}'][ind_0_maj],
+                            train_dict[f'{core_class_names[0]}_{sp_class_names[1]}'][ind_0_min]])
+    
+    data1 = np.concatenate([train_dict[f'{core_class_names[1]}_{sp_class_names[1]}'][ind_1_maj],
+                            train_dict[f'{core_class_names[1]}_{sp_class_names[0]}'][ind_1_min]])
+    
+    data_np = np.concatenate([
+                            data0[None],
+                            data1[None],
+                            ])
+    
+    return data_np
+
+
+
+    
+
+def visualize_correlations(embeddings, ood_embeddings, core_ax, sp_ax,
+                           value_dict=None, print_logs=False):
+    
+    c_vals = []
+    s_vals = []
+    
+    for key in embeddings.keys():
+        c_vals_ = np.abs(np.dot(embeddings[key], core_ax))
+        s_vals_ = np.abs(np.dot(embeddings[key], sp_ax))
+        
+        c_vals.append(c_vals_)
+        s_vals.append(s_vals_)
+
+    c_vals_ood = []        
+    s_vals_ood = []
+
+    for key in ood_embeddings.keys():
+        c_vals_ = np.abs(np.dot(ood_embeddings[key], core_ax))
+        s_vals_ = np.abs(np.dot(ood_embeddings[key], sp_ax))
+        
+        c_vals_ood.append(c_vals_)
+        s_vals_ood.append(s_vals_)
+
+    c_vals = np.concatenate(c_vals)
+    c_vals_ood = np.concatenate(c_vals_ood)
+    s_vals = np.concatenate(s_vals)
+    s_vals_ood = np.concatenate(s_vals_ood)
+
+    
+    plt.figure(figsize=(8,4))
+    plt.subplot(121)
+    plt.hist(c_vals, 25, histtype='step', density=False, linewidth=2.5, label='embs', color='tab:blue')
+    plt.hist(c_vals_ood, 25, histtype='step', density=False, linewidth=2.5, label='ood', color='tab:orange')
+    plt.title('core alignment')
+    plt.subplot(122)
+    plt.hist(s_vals, 25, histtype='step', density=False, linewidth=2.5, label='embs', color='tab:blue')
+    plt.hist(s_vals_ood, 25, histtype='step', density=False, linewidth=2.5, label='ood', color='tab:orange')
+    plt.title('sp alignment')
+    plt.legend()
+    
+    if value_dict is not None:
+        plt.subplot(121)
+        plt.hist(value_dict['c_vals'], 25, histtype='step', linestyle='dotted',
+                 density=True, linewidth=2.5, label='embs (before)', color='tab:blue')
+        plt.hist(value_dict['c_vals_ood'], 25, histtype='step', linestyle='dotted',
+                 density=True, linewidth=2.5, label='ood (before)', color='tab:orange')
+        plt.title('core alignment')
+        plt.subplot(122)
+        plt.hist(value_dict['s_vals'], 25, histtype='step', linestyle='dotted',
+                 density=True, linewidth=2.5, label='embs (before)', color='tab:blue')
+        plt.hist(value_dict['s_vals_ood'], 25, histtype='step', linestyle='dotted',
+                 density=True, linewidth=2.5, label='ood (before)', color='tab:orange')
+        plt.title('sp alignment')
+        plt.legend()
+    
+    if print_logs:
+        print(f'core coefs ratio: {np.mean(c_vals) / np.mean(c_vals_ood)}')
+        print(f'sp coefs ratio: {np.mean(s_vals) / np.mean(s_vals_ood)}')
+        
+    if value_dict is None:
+        value_dict = {}
+        value_dict['c_vals'] = c_vals
+        value_dict['c_vals_ood'] = c_vals_ood
+        value_dict['s_vals'] = s_vals
+        value_dict['s_vals_ood'] = s_vals_ood
+        
+        return value_dict
+
+def get_axis(embeddings):
+    
+    core_ax1 = normalize(embeddings[f'{core_class_names[1]}_{sp_class_names[0]}'].mean(0, keepdims=False) - \
+                         embeddings[f'{core_class_names[0]}_{sp_class_names[0]}'].mean(0, keepdims=False))
+    core_ax2 = normalize(embeddings[f'{core_class_names[1]}_{sp_class_names[1]}'].mean(0, keepdims=False) - \
+                         embeddings[f'{core_class_names[0]}_{sp_class_names[1]}'].mean(0, keepdims=False))
+    core_ax = 0.5 * core_ax1 + 0.5 * core_ax2
+    
+    sp_ax1 = normalize(embeddings[f'{core_class_names[0]}_{sp_class_names[1]}'].mean(0, keepdims=False) - \
+                         embeddings[f'{core_class_names[0]}_{sp_class_names[0]}'].mean(0, keepdims=False))
+    sp_ax2 = normalize(embeddings[f'{core_class_names[1]}_{sp_class_names[1]}'].mean(0, keepdims=False) - \
+                         embeddings[f'{core_class_names[1]}_{sp_class_names[0]}'].mean(0, keepdims=False))
+    sp_ax = 0.5 * sp_ax1 + 0.5 * sp_ax2
+    
+    return core_ax, sp_ax
+
+
+def get_embeddings(model, group_data, max_l=32):
+    
+    model.eval()
+    embeddings = {}
+    
+    for key in group_data.keys():
+        with torch.no_grad():
+            data_np = group_data[key]
+            data = torch.tensor(data_np, dtype=torch.float32).to(device)
+            if len(data) > max_l:
+                features = []
+                for b in range(len(data) // max_l):
+                    batch_data = data[b * max_l: (b + 1) * max_l]
+                    feats = model(batch_data.to(device)) 
+                    features.append(feats)
+                features = torch.cat(features)
+            else:
+                features = model(data.to(device))
+        embeddings[key] = features.squeeze()
+
+    return embeddings
+    
+
+    
+core_ax, sp_ax = get_axis(train_dict)
+print('ax correlation: ', np.dot(core_ax, sp_ax))
+_ = visualize_correlations(test_dict, ood_dict, core_ax, sp_ax)
+
+core_ax_torch = torch.tensor(core_ax, dtype=torch.float32).to(device)
+sp_ax_torch = torch.tensor(sp_ax, dtype=torch.float32).to(device)
+
+mlp = nn_utils.MLP(n_feats).to(device)  
+model = nn_utils.ProtoNet(mlp, device)
+
+loss_function = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
+    
+for e in range(n_steps):
+    
+    optimizer.zero_grad()
+    
+    support_np = sample_data(n_support, sp_rate=0.5)
+    query_np = sample_data(n_query, sp_rate=sp_rate)
+    data_np = np.concatenate([support_np, query_np], 1)
+    
+    data = torch.from_numpy(data_np).float().to(device)
+    
+    sample_dict = {
+        'data': data,
+        'n_way': n_way,
+        'n_support': n_support,
+        'n_query': n_query
+        }
+        
+    loss, output = model.set_forward_loss(sample_dict)
+        
+    loss.backward()
+    optimizer.step()
+    if e > 2:
+        mlp.eval()
+        
+        with torch.no_grad():
+            
+            support_np = sample_data(n_support, sp_rate=0.5)
+            query_np = sample_data(n_query_test, sp_rate=0.5)
+            data_np = np.concatenate([support_np, query_np], 1)
+            
+            data = torch.from_numpy(data_np).float().to(device)
+            
+            sample_dict = {
+                'data': data,
+                'n_way': n_way,
+                'n_support': n_support,
+                'n_query': n_query_test
+                }
+                
+            loss, output = model.set_forward_loss(sample_dict)
+            print('acc; ', output['acc'])
+
+        train_emb_dict = get_embeddings(mlp, train_dict)
+        test_emb_dict = get_embeddings(mlp, test_dict)
+        ood_emb_dict = get_embeddings(mlp, ood_dict)
+        
+        core_ax, sp_ax = get_axis(train_emb_dict)
+        print('ax correlation: ', np.dot(core_ax, sp_ax))
+        
+        
+        core_ax_torch = torch.tensor(core_ax, dtype=torch.float32).to(device)
+        sp_ax_torch = torch.tensor(sp_ax, dtype=torch.float32).to(device)
+        
+        if e % 5 == 0:
+            _ = visualize_correlations(test_emb_dict, ood_emb_dict, core_ax, sp_ax)
+        
+        mlp.train()
