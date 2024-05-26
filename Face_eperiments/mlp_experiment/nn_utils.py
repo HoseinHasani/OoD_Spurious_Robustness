a9e7bb76d0dd220b70f8e80da883f8f2abef6184
hosein <hosein.hasani@sharif.edu>
2024-05-26 15:26:07 +0330

    Prototypical experiment for CelebA

---
+++ Face_eperiments/mlp_experiment/nn_utils.py
@@ -0,0 +1,111 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.autograd import Variable
+
+
+class MLP(nn.Module):
+    def __init__(self, n_feat=1024, n_outputs=1024, input_scale=1., sigmoid_output=False):
+        super().__init__()
+        
+        self.input_scale = input_scale
+        
+        self.layer1 = nn.Linear(n_feat, n_feat)
+        self.layer2 = nn.Linear(n_feat, n_feat)
+        self.layer3 = nn.Linear(n_feat, n_outputs)
+        #self.dropout = nn.Dropout(p=0.5)
+        
+        self.sigmoid_output = sigmoid_output
+
+    def forward(self, x):
+        x1 = self.input_scale * F.relu(self.layer1(x))
+        x2 = F.relu(self.layer2(x1))
+        if self.sigmoid_output:
+            x3 = F.sigmoid(self.layer3(x2))
+            return x3
+        else:
+            return x2
+     
+
+
+class ProtoNet(nn.Module):
+  def __init__(self, encoder, device):
+    """
+    Args:
+        encoder : CNN encoding the images in sample
+        n_way (int): number of classes in a classification task
+        n_support (int): number of labeled examples per class in the support set
+        n_query (int): number of labeled examples per class in the query set
+    """
+    self.device = device
+    super(ProtoNet, self).__init__()
+    self.encoder = encoder.to(self.device)
+
+
+  def set_forward_loss(self, sample):
+    """
+    Computes loss, accuracy and output for classification task
+    Args:
+        sample (torch.Tensor): shape (n_way, n_support+n_query, (dim)) 
+    Returns:
+        torch.Tensor: shape(2), loss, accuracy and y_hat
+    """
+    sample_images = sample['data'].to(self.device)
+    n_way = sample['n_way']
+    n_support = sample['n_support']
+    n_query = sample['n_query']
+
+    x_support = sample_images[:, :n_support]
+    x_query = sample_images[:, n_support:]
+   
+    #target indices are 0 ... n_way-1
+    target_inds = torch.arange(0, n_way).view(n_way, 1, 1).expand(n_way, n_query, 1).long()
+    target_inds = Variable(target_inds, requires_grad=False)
+    target_inds = target_inds.to(self.device)
+   
+    #encode images of the support and the query set
+    x = torch.cat([x_support.contiguous().view(n_way * n_support, *x_support.size()[2:]),
+                   x_query.contiguous().view(n_way * n_query, *x_query.size()[2:])], 0)
+   
+    z = self.encoder.forward(x)
+    z_dim = z.size(-1) #usually 64
+    z_proto = z[:n_way*n_support].view(n_way, n_support, z_dim).mean(1)
+    z_query = z[n_way*n_support:]
+
+    #compute distances
+    dists = euclidean_dist(z_query, z_proto)
+    
+    #compute probabilities
+    log_p_y = F.log_softmax(-dists, dim=1).view(n_way, n_query, -1)
+   
+    loss_val = -log_p_y.gather(2, target_inds).squeeze().view(-1).mean()
+    _, y_hat = log_p_y.max(2)
+    acc_val = torch.eq(y_hat, target_inds.squeeze()).float().mean()
+   
+    return loss_val, {
+        'loss': loss_val.item(),
+        'acc': acc_val.item(),
+        'y_hat': y_hat,
+        'log_p_y': log_p_y
+        }
+     
+
+
+def euclidean_dist(x, y):
+  """
+  Computes euclidean distance btw x and y
+  Args:
+      x (torch.Tensor): shape (n, d). n usually n_way*n_query
+      y (torch.Tensor): shape (m, d). m usually n_way
+  Returns:
+      torch.Tensor: shape(n, m). For each query, the distances to each centroid
+  """
+  n = x.size(0)
+  m = y.size(0)
+  d = x.size(1)
+  assert d == y.size(1)
+
+  x = x.unsqueeze(1).expand(n, m, d)
+  y = y.unsqueeze(0).expand(n, m, d)
+
+  return torch.pow(x - y, 2).sum(2)
