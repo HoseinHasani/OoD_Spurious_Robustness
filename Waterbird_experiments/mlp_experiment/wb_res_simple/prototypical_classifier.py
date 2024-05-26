5ce1fb3f8717ab30befdc88b599d16032b9840e4
Hosein Hasani <hasanih@ce.sharif.edu>
2024-05-26 21:01:51 +0330

    Prototypical JTT on ResNet

--- Waterbird_experiments/mlp_experiment/wb_res_simple/prototypical_classifier.py
+++ Waterbird_experiments/mlp_experiment/wb_res_simple/prototypical_classifier.py
@@ -1,5 +1,7 @@
 import numpy as np
 from sklearn.metrics import accuracy_score
+from sklearn.linear_model import LogisticRegression
+
 
 x_train = np.load('x_train.npy')
 y_train = np.load('y_train.npy')
@@ -19,6 +21,45 @@ for key in test_dict.keys():
     
     dists = np.linalg.norm(data[..., None] - prototypes[None], axis=1)
     preds = np.argmin(dists, -1)    
+    
+    acc = np.round(accuracy_score(label, preds), 5)
+    print(f'group: {key}, acc: {acc}')
+    
+    
+if False:
+    clf = LogisticRegression()
+    
+    clf.fit(x_train, y_train)
+    y_hat_train = clf.predict(x_train)
+else:
+    dists = np.linalg.norm(x_train[..., None] - prototypes[None], axis=1)
+    y_hat_train = np.argmin(dists, -1)  
+
+total_miss_inds = np.argwhere(y_hat_train != y_train).ravel()
+total_crr_inds = np.argwhere(y_hat_train == y_train).ravel()
+print(np.intersect1d(total_crr_inds, total_miss_inds, assume_unique=True))
+
+prototypes = []
+
+for l in [0, 1]:
+    class_inds = np.argwhere(y_train == l).ravel()
+    class_miss_inds = np.intersect1d(class_inds, total_miss_inds, assume_unique=True)
+    prototypes.append(x_train[class_miss_inds].mean(0))
+    class_crr_inds = np.intersect1d(class_inds, total_crr_inds, assume_unique=True)
+    prototypes.append(x_train[class_crr_inds].mean(0))
+
+prototypes = np.array(prototypes).T
+
+
+for key in test_dict.keys():
+    data = test_dict[key]
+    label = float(key[:1]) * np.ones(len(data))
+    
+    dists = np.linalg.norm(data[..., None] - prototypes[None], axis=1)
+    preds = np.argmin(dists, -1)    
+    
+    preds[preds < 1.5] = 0
+    preds[preds > 1] = 1
     
     acc = np.round(accuracy_score(label, preds), 5)
     print(f'group: {key}, acc: {acc}')
