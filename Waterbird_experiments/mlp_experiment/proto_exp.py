a97f6b8167e72a7428d67a0053289ba37db98ae1
hosein <hosein.hasani@sharif.edu>
2024-05-28 00:22:19 +0330

    Distance-based OoD detection

---
+++ Waterbird_experiments/mlp_experiment/proto_exp.py
@@ -0,0 +1,175 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LogisticRegression
+import dist_utils
+import os
+import warnings
+
+warnings.filterwarnings("ignore")
+
+normalize_embs = True
+
+
+backbones = ['dino', 'res50', 'res18']
+backbone = backbones[1]
+resnet_types = ['pretrained', 'finetuned', 'scratch']
+resnet_type = resnet_types[0]
+
+core_class_names = ['0', '1']
+ood_class_names = ['0', '1']
+sp_class_names = ['0', '1']
+place_names = ['land', 'water']
+
+data_path = '../embeddings/'
+
+
+
+if backbone == 'dino':
+    in_data_embs0 = np.load(data_path + 'waterbird_embs_DINO.npy', allow_pickle=True).item()
+elif backbone == 'res50':
+    in_data_embs0 = np.load(data_path + f'wb_embs_{backbone}_{resnet_type}.npy', allow_pickle=True).item()
+
+ood_embs0 = {}
+if backbone == 'dino':
+    dict_ = np.load(data_path + 'OOD_land_DINO_eval.npy', allow_pickle=True).item()
+elif backbone == 'res50':
+    dict_ = np.load(data_path + f'OOD_land_res50_eval.npy', allow_pickle=True).item()
+    
+ood_embs0['0'] = np.array([dict_[key].squeeze() for key in dict_.keys()])
+
+if backbone == 'dino':
+    dict_ = np.load(data_path + 'OOD_water_DINO_eval.npy', allow_pickle=True).item()
+elif backbone == 'res50':
+    dict_ = np.load(data_path + f'OOD_water_res50_eval.npy', allow_pickle=True).item()
+ood_embs0['1'] = np.array([dict_[key].squeeze() for key in dict_.keys()])
+
+grouped_embs0 = {}
+grouped_embs_train0 = {}
+
+for key in in_data_embs0.keys():
+    emb = in_data_embs0[key].squeeze()
+    label = key[0]
+    place = key[2]
+    split = key[4]
+    name = f'{label}_{place}'
+    
+    if split != '0':
+        if name not in grouped_embs0.keys():
+            grouped_embs0[name] = []
+        
+        grouped_embs0[name].append(emb)
+    else:
+        if name not in grouped_embs_train0.keys():
+            grouped_embs_train0[name] = []
+        
+        grouped_embs_train0[name].append(emb)
+
+grouped_embs0 = {name: np.array(grouped_embs0[name]) for name in grouped_embs0.keys()}
+grouped_embs_train0 = {name: np.array(grouped_embs_train0[name]) for name in grouped_embs_train0.keys()}
+
+
+
+def normalize(x):
+    return x / np.linalg.norm(x, axis=-1, keepdims=True)
+
+
+grouped_embs = {name: grouped_embs0[name] for name in grouped_embs0.keys()}
+grouped_embs_train = {name: grouped_embs_train0[name] for name in grouped_embs_train0.keys()}
+ood_embs = {name: ood_embs0[name] for name in ood_embs0.keys()}
+
+
+if normalize_embs:
+    train_dict = {key: normalize(grouped_embs_train0[key]) for key in grouped_embs_train0.keys()}
+    test_dict = {key: normalize(grouped_embs0[key]) for key in grouped_embs0.keys()}
+    ood_dict = {key: normalize(ood_embs0[key]) for key in ood_embs0.keys()}
+else:
+    train_dict = grouped_embs_train0
+    test_dict = grouped_embs
+    ood_dict = ood_embs0
+
+
+
+
+
+def get_axis(embeddings):
+    
+    core_ax1 = embeddings[f'{core_class_names[1]}_{sp_class_names[0]}'].mean(0, keepdims=False) - \
+                         embeddings[f'{core_class_names[0]}_{sp_class_names[0]}'].mean(0, keepdims=False)
+    core_ax2 = embeddings[f'{core_class_names[1]}_{sp_class_names[1]}'].mean(0, keepdims=False) - \
+                         embeddings[f'{core_class_names[0]}_{sp_class_names[1]}'].mean(0, keepdims=False)
+    core_ax = 0.5 * core_ax1 + 0.5 * core_ax2
+    
+    sp_ax1 = embeddings[f'{core_class_names[0]}_{sp_class_names[1]}'].mean(0, keepdims=False) - \
+                         embeddings[f'{core_class_names[0]}_{sp_class_names[0]}'].mean(0, keepdims=False)
+    sp_ax2 = embeddings[f'{core_class_names[1]}_{sp_class_names[1]}'].mean(0, keepdims=False) - \
+                         embeddings[f'{core_class_names[1]}_{sp_class_names[0]}'].mean(0, keepdims=False)
+    sp_ax = 0.5 * sp_ax1 + 0.5 * sp_ax2
+    
+    print('axis ratio:', np.linalg.norm(core_ax) / np.linalg.norm(sp_ax))
+    
+    core_ax_norm = 0.5 * normalize(core_ax1) + 0.5 * normalize(core_ax2)
+    sp_ax_norm = 0.5 * normalize(sp_ax1) + 0.5 * normalize(sp_ax2)
+    
+    return core_ax, sp_ax, core_ax_norm, sp_ax_norm
+
+
+
+def get_class_dicts(input_dict):
+    class_dicts = []
+    for core_name in core_class_names:
+        class_dict = {}
+        for sp_name in sp_class_names:
+            name = f'{core_name}_{sp_name}'
+            class_dict[name] = input_dict[name]
+        class_dicts.append(class_dict)
+
+    return class_dicts
+
+
+def plot_dict_hist(dict_data, fig_name):
+    data = []
+    for name in dict_data:
+        data.append(dict_data[name])
+    
+    data = np.concatenate(data)
+    
+    plt.hist(data, 100, histtype='step', linewidth=1.5, label=fig_name)
+    plt.legend()
+
+    
+core_ax, sp_ax, core_ax_norm, sp_ax_norm = get_axis(train_dict)
+print('ax correlation: ', np.dot(core_ax, sp_ax))
+
+
+    
+print()
+dist_utils.calc_dists_ratio(train_dict, ood_dict)
+dist_utils.calc_dists_ratio(test_dict, ood_dict)
+
+train_dict_list = get_class_dicts(train_dict)
+test_dict_list = get_class_dicts(test_dict)
+
+ood_embs = np.concatenate([ood_dict[key] for key in ood_dict.keys()])
+
+train_prototypes = []
+train_embs = []
+for data in train_dict_list:
+    all_data = []
+    for key in data.keys():
+        all_data.append(data[key])
+        
+    all_data = np.concatenate(all_data)
+    train_embs.append(all_data)
+    train_prototypes.append(all_data.mean(0))
+
+
+#train_prototypes = [train_dict[key].mean(0) for key in train_dict.keys()]
+train_prototypes = np.array(train_prototypes)
+
+print('OOD:')
+#dist_utils.calc_ROC(test_dict_list[0], ood_embs, prototypes=train_dict_list[0])
+#dist_utils.calc_ROC(test_dict_list[1], ood_embs, prototypes=train_dict_list[1])
+dist_utils.calc_ROC(test_dict, ood_embs, prototypes=train_prototypes)
+
+
+
