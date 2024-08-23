35dbe494727ef0d863aa9d7b7fabd6b055f3478f
Hosein <hosein.hasani@sharif.edu>
2024-08-23 18:39:32 +0330

    Generate synthetic images with FLUX.1-dev

---
+++ Image_generation/wb.py
@@ -0,0 +1,17 @@
+import torch
+from diffusers import FluxPipeline
+
+pipe = FluxPipeline.from_pretrained("black-forest-labs/FLUX.1-dev", torch_dtype=torch.bfloat16)
+pipe.enable_model_cpu_offload()
+
+prompt = "A big camel walking on a beach"
+image = pipe(
+    prompt,
+    height=512,
+    width=512,
+    guidance_scale=3.5,
+    num_inference_steps=50,
+    max_sequence_length=512,
+    generator=torch.Generator("cpu").manual_seed(0)
+).images[0]
+image.save("camel_beach.png")
