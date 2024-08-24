dcaef9dc7ec448965763007e1af3ca7fff7d6d8d
Hosein <hosein.hasani@sharif.edu>
2024-08-24 14:48:52 +0330

    Resolve conflicts

--- Image_generation/wb_api.py
+++ Image_generation/wb_api.py
@@ -1,8 +1,6 @@
 from gradio_client import Client
 import numpy as np
 
-<<<<<<< HEAD
-
 seed = np.random.randint(0, 10)
 steps = np.random.randint(2, 6)
 print(seed, steps)
@@ -13,12 +11,6 @@ client = Client("black-forest-labs/FLUX.1-schnell", download_files="pics")
 result = client.predict(
 		prompt=prompt,
 		seed=seed,
-=======
-client = Client("black-forest-labs/FLUX.1-schnell", download_files="pics")
-result = client.predict(
-		prompt="A Cow walking in grass field",
-		seed=0,
->>>>>>> origin/main
 		randomize_seed=True,
 		width=1024,
 		height=1024,
