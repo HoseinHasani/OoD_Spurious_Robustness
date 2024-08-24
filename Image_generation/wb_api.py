b4f1f9423d4cc75423eb5cd18d1116c70af29941
Hosein <hosein.hasani@sharif.edu>
2024-08-24 14:34:56 +0330

    Change default path

--- Image_generation/wb_api.py
+++ Image_generation/wb_api.py
@@ -1,13 +1,21 @@
 from gradio_client import Client
+import numpy as np
 
-client = Client("black-forest-labs/FLUX.1-schnell")
+
+seed = np.random.randint(0, 10)
+steps = np.random.randint(2, 6)
+print(seed, steps)
+prompt = "A german shepherd beside a woman"
+prompt = "A Doberman beside a man"
+
+client = Client("black-forest-labs/FLUX.1-schnell", download_files="pics")
 result = client.predict(
-		prompt="A big camel walking on a beach",
-		seed=0,
+		prompt=prompt,
+		seed=seed,
 		randomize_seed=True,
 		width=1024,
 		height=1024,
-		num_inference_steps=4,
+		num_inference_steps=steps,
 		api_name="/infer"
 )
 print(result)
