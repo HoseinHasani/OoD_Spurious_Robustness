168904e7c3e1d59e5ca61a29720a3d946617df8b
Hosein <hosein.hasani@sharif.edu>
2024-10-16 18:55:02 +0330

    Flux dev to schnell

--- Image_generation/local.py
+++ Image_generation/local.py
@@ -3,33 +3,237 @@ from diffusers import FluxPipeline
 import os 
 import numpy as np
 import time
+nn = 100
+nnn = 1
 
-prompt = "A cow in a grass field"
-pic_path = f"data/{prompt}"
+min_step = 4
+max_step = 8
 
-os.makedirs(pic_path, exist_ok=True)
+#pipe = FluxPipeline.from_pretrained("black-forest-labs/FLUX.1-dev", torch_dtype=torch.bfloat16)
+pipe = FluxPipeline.from_pretrained("black-forest-labs/FLUX.1-schnell", torch_dtype=torch.bfloat16)
 
-pipe = FluxPipeline.from_pretrained("black-forest-labs/FLUX.1-dev", torch_dtype=torch.bfloat16)
 pipe = pipe.to("cuda")
-
-for i in range(10):
     
-    t0 = time.time()
-    seed = int(t0) % 1000000
-    np.random.seed(seed)
-
-    num_inference_steps = np.random.randint(30, 70)
-    guidance_scale = 3 + 5 * np.random.rand()
-
-    image = pipe(prompt,
-                height=224,
-                width=224,
-                guidance_scale=guidance_scale,
-                num_inference_steps=num_inference_steps,
-                max_sequence_length=256,
-                generator=torch.Generator("cuda").manual_seed(seed + 5)
-                 ).images[0]
     
-    image.save(f"{pic_path}/{i}_{seed}.png")
+for j in range(40, nn):
+
+    prompt = "Picture of a desert"
+    pic_path = f"data/{prompt}"
     
-    print(i, int(time.time() - t0), seed, num_inference_steps)
+    os.makedirs(pic_path, exist_ok=True)
+    
+
+    for i in range(nnn):
+        
+        t0 = time.time()
+        seed = int(t0) % 1000000
+        np.random.seed(seed)
+    
+        num_inference_steps = np.random.randint(min_step, max_step)
+        guidance_scale = 3 + 5 * np.random.rand()
+    
+        image = pipe(prompt,
+                    height=224,
+                    width=224,
+                    guidance_scale=guidance_scale,
+                    num_inference_steps=num_inference_steps,
+                    max_sequence_length=256,
+                    generator=torch.Generator("cuda").manual_seed(seed + 5)
+                     ).images[0]
+        
+        image.save(f"{pic_path}/{j*nn + i}_{seed}.png")
+        
+        print(i, int(time.time() - t0), seed, num_inference_steps)
+
+    prompt = "Picture of a grassland"
+    pic_path = f"data/{prompt}"
+    
+    os.makedirs(pic_path, exist_ok=True)
+    
+    for i in range(nnn):
+        
+        t0 = time.time()
+        seed = int(t0) % 1000000
+        np.random.seed(seed)
+    
+        num_inference_steps = np.random.randint(min_step, max_step)
+        guidance_scale = 3 + 5 * np.random.rand()
+    
+        image = pipe(prompt,
+                    height=224,
+                    width=224,
+                    guidance_scale=guidance_scale,
+                    num_inference_steps=num_inference_steps,
+                    max_sequence_length=256,
+                    generator=torch.Generator("cuda").manual_seed(seed + 5)
+                     ).images[0]
+        
+        image.save(f"{pic_path}/{j*nn + i}_{seed}.png")
+        
+        print(i, int(time.time() - t0), seed, num_inference_steps)
+        
+        
+    prompt = "A cow in a grassland"
+    pic_path = f"data/{prompt}"
+    
+    os.makedirs(pic_path, exist_ok=True)
+    
+    for i in range(4*nnn):
+        
+        t0 = time.time()
+        seed = int(t0) % 1000000
+        np.random.seed(seed)
+    
+        num_inference_steps = np.random.randint(min_step, max_step)
+        guidance_scale = 3 + 5 * np.random.rand()
+    
+        image = pipe(prompt,
+                    height=224,
+                    width=224,
+                    guidance_scale=guidance_scale,
+                    num_inference_steps=num_inference_steps,
+                    max_sequence_length=256,
+                    generator=torch.Generator("cuda").manual_seed(seed + 5)
+                     ).images[0]
+        
+        image.save(f"{pic_path}/{j*nn + i}_{seed}.png")
+        
+        print(i, int(time.time() - t0), seed, num_inference_steps)
+        
+        
+    prompt = "A horse in a grassland"
+    pic_path = f"data/{prompt}"
+    
+    os.makedirs(pic_path, exist_ok=True)
+    
+    for i in range(2*nnn):
+        
+        t0 = time.time()
+        seed = int(t0) % 1000000
+        np.random.seed(seed)
+    
+        num_inference_steps = np.random.randint(min_step, max_step)
+        guidance_scale = 3 + 5 * np.random.rand()
+    
+        image = pipe(prompt,
+                    height=224,
+                    width=224,
+                    guidance_scale=guidance_scale,
+                    num_inference_steps=num_inference_steps,
+                    max_sequence_length=256,
+                    generator=torch.Generator("cuda").manual_seed(seed + 5)
+                     ).images[0]
+        
+        image.save(f"{pic_path}/{j*nn + i}_{seed}.png")
+        
+        print(i, int(time.time() - t0), seed, num_inference_steps)
+        
+    prompt = "A camel in a green grassland"
+    pic_path = f"data/{prompt}"
+    
+    os.makedirs(pic_path, exist_ok=True)
+    
+    for i in range(nnn):
+        
+        t0 = time.time()
+        seed = int(t0) % 1000000
+        np.random.seed(seed)
+    
+        num_inference_steps = np.random.randint(min_step, max_step)
+        guidance_scale = 3 + 5 * np.random.rand()
+    
+        image = pipe(prompt,
+                    height=224,
+                    width=224,
+                    guidance_scale=guidance_scale,
+                    num_inference_steps=num_inference_steps,
+                    max_sequence_length=256,
+                    generator=torch.Generator("cuda").manual_seed(seed + 5)
+                     ).images[0]
+        
+        image.save(f"{pic_path}/{j*nn + i}_{seed}.png")
+        
+        print(i, int(time.time() - t0), seed, num_inference_steps)
+        
+        
+    prompt = "A cow in a desert"
+    pic_path = f"data/{prompt}"
+    
+    os.makedirs(pic_path, exist_ok=True)
+    
+    for i in range(nnn):
+        
+        t0 = time.time()
+        seed = int(t0) % 1000000
+        np.random.seed(seed)
+    
+        num_inference_steps = np.random.randint(min_step, max_step)
+        guidance_scale = 3 + 5 * np.random.rand()
+    
+        image = pipe(prompt,
+                    height=224,
+                    width=224,
+                    guidance_scale=guidance_scale,
+                    num_inference_steps=num_inference_steps,
+                    max_sequence_length=256,
+                    generator=torch.Generator("cuda").manual_seed(seed + 5)
+                     ).images[0]
+        
+        image.save(f"{pic_path}/{j*nn + i}_{seed}.png")
+        
+        print(i, int(time.time() - t0), seed, num_inference_steps)
+        
+        
+    prompt = "A horse in a desert"
+    pic_path = f"data/{prompt}"
+    
+    os.makedirs(pic_path, exist_ok=True)
+    
+    for i in range(2*nnn):
+        
+        t0 = time.time()
+        seed = int(t0) % 1000000
+        np.random.seed(seed)
+    
+        num_inference_steps = np.random.randint(min_step, max_step)
+        guidance_scale = 3 + 5 * np.random.rand()
+    
+        image = pipe(prompt,
+                    height=224,
+                    width=224,
+                    guidance_scale=guidance_scale,
+                    num_inference_steps=num_inference_steps,
+                    max_sequence_length=256,
+                    generator=torch.Generator("cuda").manual_seed(seed + 5)
+                     ).images[0]
+        
+        image.save(f"{pic_path}/{j*nn + i}_{seed}.png")
+        
+        print(i, int(time.time() - t0), seed, num_inference_steps)
+        
+    prompt = "A camel in a desert"
+    pic_path = f"data/{prompt}"
+    
+    os.makedirs(pic_path, exist_ok=True)
+    
+    for i in range(4*nnn):
+        
+        t0 = time.time()
+        seed = int(t0) % 1000000
+        np.random.seed(seed)
+    
+        num_inference_steps = np.random.randint(min_step, max_step)
+        guidance_scale = 3 + 5 * np.random.rand()
+    
+        image = pipe(prompt,
+                    height=224,
+                    width=224,
+                    guidance_scale=guidance_scale,
+                    num_inference_steps=num_inference_steps,
+                    max_sequence_length=256,
+                    generator=torch.Generator("cuda").manual_seed(seed + 5)
+                     ).images[0]
+        
+        image.save(f"{pic_path}/{j*nn + i}_{seed}.png")
+        
+        print(i, int(time.time() - t0), seed, num_inference_steps)
