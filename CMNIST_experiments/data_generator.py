3c6d61a1357f60d5ce1faa1a11177a6d42a265b5
Hosein <hosein.hasani@sharif.edu>
2024-11-04 21:28:06 +0330

    Consider OOD samples

--- CMNIST_experiments/data_generator.py
+++ CMNIST_experiments/data_generator.py
@@ -1,50 +1,50 @@
+import torch
+from torchvision import datasets, transforms
+from torch.utils.data import Dataset
 import numpy as np
 
 def generate_distinct_colors(num_colors=5, min_distance=100):
     colors = []
-
     def l2_distance(color1, color2):
-        return np.linalg.norm(np.array(color1) - np.array(color2))
-
+        return torch.norm(color1 - color2).item()
+    
     while len(colors) < num_colors:
-        candidate = np.random.randint(0, 256, 3)  # Generate a random RGB color
-        if all(l2_distance(candidate, color) >= min_distance for color in colors):
+        candidate = torch.randint(0, 256, (3,)).float()  # Random RGB color
+        if all(l2_distance(candidate, torch.tensor(color)) >= min_distance for color in colors):
             colors.append(candidate)
-
-    return np.array(colors)
-
-colors = generate_distinct_colors()
-print("Generated Colors:", colors)
-
+    
+    return torch.stack(colors)  
 
 class ColoredMNIST(Dataset):
-    def __init__(self, train=True, transform=None, min_distance=100):
+    def __init__(self, colors, train=True, transform=None):
         self.mnist = datasets.MNIST(root='./data', train=train, download=True)
         self.transform = transform
-
-        self.colors = generate_distinct_colors(num_colors=5, min_distance=min_distance)
+        self.colors = colors
 
     def __len__(self):
         return len(self.mnist)
 
     def __getitem__(self, idx):
         img, label = self.mnist[idx]
-
-        if label >= 5:
-            return self.__getitem__((idx + 1) % len(self))  # Skip this item, use modulo to prevent out-of-bounds
-
+        
         img_rgb = torch.cat([transforms.ToTensor()(img)] * 3, dim=0)
-
-        background_color = self.colors[label]
-
+        
+        if label < 5:
+            background_color = self.colors[label]  
+            in_distribution = True
+        else:
+            background_color = self.colors[label % 5] 
+            in_distribution = False
+        
         img_colored = background_color[:, None, None] * torch.ones_like(img_rgb)
         img_colored = torch.where(img_rgb > 0, img_rgb, img_colored)
 
         if self.transform:
             img_colored = self.transform(img_colored)
+        
+        return img_colored, label, in_distribution
 
-        return img_colored, label
+colors = generate_distinct_colors(num_colors=5, min_distance=100)
 
-
-train_dataset = ColoredMNIST(train=True, transform=transforms.ToTensor())
-validation_dataset = ColoredMNIST(train=False, transform=transforms.ToTensor())
+train_dataset = ColoredMNIST(colors=colors, train=True, transform=transforms.ToTensor())
+validation_dataset = ColoredMNIST(colors=colors, train=False, transform=transforms.ToTensor())
