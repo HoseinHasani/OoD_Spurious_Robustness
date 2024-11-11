5715a5db0cdcbcf55d85680481f2d94198f784ae
Hosein <hosein.hasani@sharif.edu>
2024-11-11 21:37:43 +0330

    Report the ratio

--- MetaCOCO/dataset_parser.py
+++ MetaCOCO/dataset_parser.py
@@ -1,11 +1,9 @@
 import os
 import pandas as pd
 
-
 def get_image_count_in_folder(folder_path):
     image_extensions = {'.jpg', '.jpeg', '.png', '.gif', '.bmp', '.tiff'}
     return sum(1 for file in os.listdir(folder_path) if os.path.splitext(file)[1].lower() in image_extensions)
-
 
 def parse_dataset_folder(root_folder):
     classes = {}
@@ -37,14 +35,23 @@ def parse_dataset_folder(root_folder):
     df['Total'] = df.sum(axis=1)
     df.loc['Total'] = df.sum(axis=0)
 
-    return df
+    ratio_df = df.copy()
 
+    for class_name in df.columns[:-1]:  
+        ratio_df[class_name] = (df[class_name] / df[class_name].sum() * 200).round().astype(int)
 
-def save_stats_to_csv(root_folder, output_csv='dataset_statistics.csv'):
-    df = parse_dataset_folder(root_folder)
+    ratio_df['Total'] = (df['Total'] / df['Total'].sum() * 100).round().astype(int)
+
+    return df, ratio_df
+
+def save_stats_to_csv(root_folder, output_csv='dataset_statistics.csv', ratio_csv='dataset_ratio_statistics.csv'):
+    df, ratio_df = parse_dataset_folder(root_folder)
+    
     df.to_csv(output_csv)
     print(f"Statistics saved to {output_csv}")
+    
+    ratio_df.to_csv(ratio_csv)
+    print(f"Ratio statistics saved to {ratio_csv}")
 
-
-root_folder = 'FSLSHIFT'
+root_folder = 'FSLSHIFT'  
 save_stats_to_csv(root_folder)
