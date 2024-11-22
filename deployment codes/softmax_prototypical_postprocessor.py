6dbad0e23aeae0b5abaf84404af5e567ce79767c
Hosein <hosein.hasani@sharif.edu>
2024-11-23 02:07:04 +0330

    Reform postprocess codes

--- deployment codes/softmax_prototypical_postprocessor.py
+++ deployment codes/softmax_prototypical_postprocessor.py
@@ -7,90 +7,22 @@ import torch.nn as nn
 from tqdm import tqdm
 import openood.utils.comm as comm
 from .base_postprocessor import BasePostprocessor
-from sklearn.cluster import KMeans
 
 
-class ClusterPrototypicalPostprocessor(BasePostprocessor):
+
+class PrototypicalPostprocessor(BasePostprocessor):
     def __init__(self, config):
-        super(ClusterPrototypicalPostprocessor, self).__init__(config)
+        super(PrototypicalPostprocessor, self).__init__(config)
         self.args = self.config.postprocessor.postprocessor_args
         self.activation_log = None
         self.args_dict = self.config.postprocessor.postprocessor_sweep
         self.setup_flag = False
-        self.perform_normalization = True
+        self.perform_normalization = False
         self.train_embs = []
         self.train_prototypes = []
         self.train_labels = None
         self.train_feats = None
-        
-        
-    def cluster_group_prototypes(self, group_embs, n_iter=160):
-        n_c = len(group_embs)
-        print('n clusters:', n_c)
-        kmeans = KMeans(n_clusters=n_c, max_iter=n_iter)
-        all_embs = np.concatenate(group_embs)
-        
-        kmeans.fit(all_embs)
-    
-        prototypes = kmeans.cluster_centers_
-        # print(prototypes.shape)
-        
-        return prototypes
 
-    def perform_classification(self):
-        
-        train_prototypes = np.array(self.train_prototypes.copy())
-        x_train = np.array(self.train_feats.copy())
-        y_train = np.array(self.train_labels)
-        dists = np.linalg.norm(x_train[..., None] - train_prototypes.T[None], axis=1)
-        y_hat_train = np.argmin(dists, -1)  
-        
-        
-        total_misc_inds = np.argwhere(y_hat_train != y_train).ravel()
-        total_crr_inds = np.argwhere(y_hat_train == y_train).ravel()
-        
-        aug_prototypes = []
-
-        n_c = len(np.unique(y_train))
-        
-        for l in range(n_c):
-            class_embs = []
-            class_prototypes = []
-            class_inds = np.argwhere(y_train == l).ravel()
-        
-            class_crr_inds = np.intersect1d(class_inds, total_crr_inds, assume_unique=True)
-            crr_prototype = x_train[class_crr_inds].mean(0)
-            class_prototypes.append(crr_prototype)
-            class_embs.append(x_train[class_crr_inds])
-        
-            for j in range(n_c):
-                if j == l:
-                    continue
-                
-                class_miss_inds = np.intersect1d(class_inds, total_misc_inds, assume_unique=True)
-                
-                if len(class_miss_inds) < 1:
-                    print('*'*20)
-                    print('Empty!')
-                    
-                trg_lbl_inds = np.argwhere(y_hat_train == j).ravel()
-                class_miss_inds_trg = np.intersect1d(class_miss_inds, trg_lbl_inds, assume_unique=True)
-                
-                if len(class_miss_inds_trg) > 0:
-                    trg_prototype = x_train[class_miss_inds_trg].mean(0)
-                    class_prototypes.append(trg_prototype)
-                    class_embs.append(x_train[class_miss_inds_trg])
-                    
-                
-                
-            if len(class_prototypes) > 1:
-                refined_prototypes = self.cluster_group_prototypes(class_embs)
-                aug_prototypes.extend(refined_prototypes)
-            else:
-                aug_prototypes.extend(class_prototypes)
-                
-            
-        self.train_prototypes.extend(aug_prototypes)
         
         
     def setup(self, net: nn.Module, id_loader_dict, ood_loader_dict):
@@ -124,9 +56,8 @@ class ClusterPrototypicalPostprocessor(BasePostprocessor):
             train_emb = features[inds]
             self.train_embs.append(train_emb)
             self.train_prototypes.append(train_emb.mean(0))
-            #self.train_prototypes.append(np.median(train_emb, axis=0))
+            # self.train_prototypes.append(np.median(train_emb, axis=0))
             
-        self.perform_classification()
         self.setup_flag = True
 
     @torch.no_grad()
@@ -172,11 +103,18 @@ class ClusterPrototypicalPostprocessor(BasePostprocessor):
         
         dists = np.array([self.calc_euc_dist(data, p) for p in self.train_prototypes])
         pred_list = np.argmin(dists, axis=0).astype(int)
+        
+        if True:
+            p_dist_probs = np.exp(-dists)
+            p_dist_probs = p_dist_probs / p_dist_probs.sum(0)
+            dists = 1 - p_dist_probs
+            
         conf_list = -np.min(dists, axis=0)
+        
         return pred_list, conf_list, label_list
 
-    def set_hyperparam(self, hyperparam: list):
-        self.K = hyperparam[0]
+    # def set_hyperparam(self, hyperparam: list):
+    #     self.K = hyperparam[0]
 
-    def get_hyperparam(self):
-        return self.K
+    # def get_hyperparam(self):
+    #     return self.K
