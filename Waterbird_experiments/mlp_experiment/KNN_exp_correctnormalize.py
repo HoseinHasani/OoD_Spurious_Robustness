3e7b57d31d14cc2f3bd82b40b42852338bd90bb2
Hosein <hosein.hasani@sharif.edu>
2025-05-05 00:55:22 +0330

    Train-based normalization experiment

---
+++ Waterbird_experiments/mlp_experiment/KNN_exp_correctnormalize.py
@@ -0,0 +1,188 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+import dist_utils
+import os
+import warnings
+
+warnings.filterwarnings("ignore")
+
+normalize_embs = True
+
+k_values = [0, 7, 40]
+
+backbones = ['dino', 'res50', 'res18']
+backbone = backbones[0]
+resnet_types = ['pretrained', 'finetuned', 'scratch']
+resnet_type = resnet_types[1]
+
+core_class_names = ['0', '1']
+ood_class_names = ['0', '1']
+sp_class_names = ['0', '1']
+place_names = ['land', 'water']
+
+data_path = '../embeddings/'
+
+
+
+if backbone == 'dino':
+    in_data_embs0 = np.load(data_path + 'waterbird_embs_DINO.npy', allow_pickle=True).item()
+else:
+    in_data_embs0 = np.load(data_path + f'wb_embs_{backbone}_{resnet_type}.npy', allow_pickle=True).item()
+
+ood_embs0 = {}
+if backbone == 'dino':
+    dict_ = np.load(data_path + 'OOD_land_DINO_eval.npy', allow_pickle=True).item()
+else:
+    dict_ = np.load(data_path + f'OOD_land_{backbone}_eval.npy', allow_pickle=True).item()
+    
+ood_embs0['0'] = np.array([dict_[key].squeeze() for key in dict_.keys()])
+
+if backbone == 'dino':
+    dict_ = np.load(data_path + 'OOD_water_DINO_eval.npy', allow_pickle=True).item()
+else:
+    dict_ = np.load(data_path + f'OOD_water_{backbone}_eval.npy', allow_pickle=True).item()
+ood_embs0['1'] = np.array([dict_[key].squeeze() for key in dict_.keys()])
+
+grouped_embs0 = {}
+grouped_embs_train0 = {}
+
+for key in in_data_embs0.keys():
+    emb = in_data_embs0[key].squeeze()
+    label = key[0]
+    place = key[2]
+    split = key[4]
+    name = f'{label}_{place}'
+    
+    if split != '0':
+        if name not in grouped_embs0.keys():
+            grouped_embs0[name] = []
+        
+        grouped_embs0[name].append(emb)
+    else:
+        if name not in grouped_embs_train0.keys():
+            grouped_embs_train0[name] = []
+        
+        grouped_embs_train0[name].append(emb)
+
+grouped_embs0 = {name: np.array(grouped_embs0[name]) for name in grouped_embs0.keys()}
+grouped_embs_train0 = {name: np.array(grouped_embs_train0[name]) for name in grouped_embs_train0.keys()}
+
+
+
+def normalize(x, norm, eps=1e-7):
+    return x / (norm + eps)
+
+
+grouped_embs = {name: grouped_embs0[name] for name in grouped_embs0.keys()}
+grouped_embs_train = {name: grouped_embs_train0[name] for name in grouped_embs_train0.keys()}
+ood_embs = {name: ood_embs0[name] for name in ood_embs0.keys()}
+
+train_embs = np.concatenate([grouped_embs_train0[key] for key in grouped_embs_train0.keys()])
+norm = np.linalg.norm(train_embs, axis=0, keepdims=True)
+
+if normalize_embs:
+    train_dict = {key: normalize(grouped_embs_train0[key], norm) for key in grouped_embs_train0.keys()}
+    test_dict = {key: normalize(grouped_embs0[key], norm) for key in grouped_embs0.keys()}
+    ood_dict = {key: normalize(ood_embs0[key], norm) for key in ood_embs0.keys()}
+else:
+    train_dict = grouped_embs_train0
+    test_dict = grouped_embs
+    ood_dict = ood_embs0
+
+
+
+def get_class_dicts(input_dict):
+    class_dicts = []
+    for core_name in core_class_names:
+        class_dict = {}
+        for sp_name in sp_class_names:
+            name = f'{core_name}_{sp_name}'
+            class_dict[name] = input_dict[name]
+        class_dicts.append(class_dict)
+
+    return class_dicts
+
+
+def plot_dict_hist(dict_data, fig_name):
+    data = []
+    for name in dict_data:
+        data.append(dict_data[name])
+    
+    data = np.concatenate(data)
+    
+    plt.hist(data, 100, histtype='step', linewidth=1.5, label=fig_name)
+    plt.legend()
+
+def get_nn_distances(targets, sources):
+    dists = []
+    for target in targets:
+        dists_ = [np.linalg.norm(source - target) for source in sources]
+        dists.append(np.sort(dists_)[:100])
+        
+    return np.array(dists)
+    
+def knn_classifier(dists, k=5):
+    preds = []
+    th_dists = []
+    for i in range(dists.shape[1]):
+        dists_ = dists[:, i]
+        th_val = np.sort(dists_.ravel())[k]
+        th_dists.append(th_val)
+        n_cl0 = len(np.argwhere(dists_[0] <= th_val).ravel())
+        n_cl1 = len(np.argwhere(dists_[1] <= th_val).ravel())
+        preds.append(int(n_cl1 > n_cl0))
+    
+    return np.array(preds), np.array(th_dists)
+
+    
+train_dict_list = get_class_dicts(train_dict)
+test_dict_list = get_class_dicts(test_dict)
+
+print('K-NN accuracy:')
+
+test_knn_dists = {k: [] for k in k_values}
+for key in test_dict.keys():
+    
+    targets = test_dict[key]
+    labels = float(key[0]) * np.ones(len(test_dict[key]))
+    
+    dists = []
+    for c in range(len(train_dict_list)):
+        sources = np.concatenate([train_dict_list[c][kk] for kk in train_dict_list[c].keys()])
+        dists.append(get_nn_distances(targets, sources))
+    dists = np.array(dists)
+    knn_accs = []
+    for k in k_values:
+        preds, knn_dists_ = knn_classifier(dists, k)
+        test_knn_dists[k].append(knn_dists_)
+        knn_accs.append(accuracy_score(labels, preds))
+    
+    print(key, np.round(knn_accs, 5))
+
+test_knn_dists = {k: np.concatenate(test_knn_dists[k]) for k in k_values}
+
+ood_embs = np.concatenate([ood_dict[key] for key in ood_dict.keys()])
+
+
+ood_knn_dists = {k: [] for k in k_values}
+
+dists = []
+for c in range(len(train_dict_list)):
+    sources = np.concatenate([train_dict_list[c][kk] for kk in train_dict_list[c].keys()])
+    dists.append(get_nn_distances(ood_embs, sources))
+dists = np.array(dists)
+for k in k_values:
+    preds, knn_dists_ = knn_classifier(dists, k)
+    ood_knn_dists[k].append(knn_dists_)
+
+ood_knn_dists = {k: np.concatenate(ood_knn_dists[k]) for k in k_values}
+
+
+print('OOD:')
+for k in k_values:
+    print(backbone, normalize_embs, k)
+    dist_utils.calc_ROC_with_dists(test_knn_dists[k], ood_knn_dists[k])
+
+
---
+++ Waterbird_experiments/mlp_experiment/proto_exp_correctnormalize.py
@@ -0,0 +1,335 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LogisticRegression
+import dist_utils
+import os
+import warnings
+from sklearn.cluster import KMeans
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../sprod')))
+from sprod1 import SPROD1
+
+
+warnings.filterwarnings("ignore")
+
+normalize_embs = True
+
+
+backbones = ['dino', 'res50', 'res18']
+backbone = backbones[1]
+resnet_types = ['pretrained', 'finetuned', 'scratch']
+resnet_type = resnet_types[0]
+
+core_class_names = ['0', '1']
+ood_class_names = ['0', '1']
+sp_class_names = ['0', '1']
+place_names = ['land', 'water']
+
+data_path = '../embeddings/'
+
+
+
+if backbone == 'dino':
+    in_data_embs0 = np.load(data_path + 'waterbird_embs_DINO.npy', allow_pickle=True).item()
+else:
+    in_data_embs0 = np.load(data_path + f'wb_embs_{backbone}_{resnet_type}.npy', allow_pickle=True).item()
+
+ood_embs0 = {}
+if backbone == 'dino':
+    dict_ = np.load(data_path + 'OOD_land_DINO_eval.npy', allow_pickle=True).item()
+else:
+    dict_ = np.load(data_path + f'OOD_land_{backbone}_eval.npy', allow_pickle=True).item()
+    
+ood_embs0['0'] = np.array([dict_[key].squeeze() for key in dict_.keys()])
+
+if backbone == 'dino':
+    dict_ = np.load(data_path + 'OOD_water_DINO_eval.npy', allow_pickle=True).item()
+else:
+    dict_ = np.load(data_path + f'OOD_water_{backbone}_eval.npy', allow_pickle=True).item()
+ood_embs0['1'] = np.array([dict_[key].squeeze() for key in dict_.keys()])
+
+grouped_embs0 = {}
+grouped_embs_train0 = {}
+grouped_embs_val0 = {}
+
+for key in in_data_embs0.keys():
+    emb = in_data_embs0[key].squeeze()
+    label = key[0]
+    place = key[2]
+    split = key[4]
+    name = f'{label}_{place}'
+    
+    if split == '2':
+        if name not in grouped_embs0.keys():
+            grouped_embs0[name] = []
+        
+        grouped_embs0[name].append(emb)
+    elif split == '1':
+        if name not in grouped_embs_val0.keys():
+            grouped_embs_val0[name] = []
+        
+        grouped_embs_val0[name].append(emb)
+    else:
+        if name not in grouped_embs_train0.keys():
+            grouped_embs_train0[name] = []
+        
+        grouped_embs_train0[name].append(emb)
+
+grouped_embs0 = {name: np.array(grouped_embs0[name]) for name in grouped_embs0.keys()}
+grouped_embs_train0 = {name: np.array(grouped_embs_train0[name]) for name in grouped_embs_train0.keys()}
+grouped_embs_val0 = {name: np.array(grouped_embs_val0[name]) for name in grouped_embs_val0.keys()}
+
+
+grouped_embs0 = {name: np.array(grouped_embs0[name]) for name in grouped_embs0.keys()}
+grouped_embs_train0 = {name: np.array(grouped_embs_train0[name]) for name in grouped_embs_train0.keys()}
+
+
+
+def normalize(x, norm, eps=1e-7):
+    return x / (norm + eps)
+
+
+grouped_embs = {name: grouped_embs0[name] for name in grouped_embs0.keys()}
+grouped_embs_train = {name: grouped_embs_train0[name] for name in grouped_embs_train0.keys()}
+ood_embs = {name: ood_embs0[name] for name in ood_embs0.keys()}
+
+train_embs = np.concatenate([grouped_embs_train0[key] for key in grouped_embs_train0.keys()])
+norm = np.linalg.norm(train_embs, axis=0, keepdims=True)
+
+if normalize_embs:
+    train_dict = {key: normalize(grouped_embs_train0[key], norm) for key in grouped_embs_train0.keys()}
+    test_dict = {key: normalize(grouped_embs0[key], norm) for key in grouped_embs0.keys()}
+    ood_dict = {key: normalize(ood_embs0[key], norm) for key in ood_embs0.keys()}
+else:
+    train_dict = grouped_embs_train0
+    test_dict = grouped_embs
+    ood_dict = ood_embs0
+
+
+
+
+def get_class_dicts(input_dict):
+    class_dicts = []
+    for core_name in core_class_names:
+        class_dict = {}
+        for sp_name in sp_class_names:
+            name = f'{core_name}_{sp_name}'
+            class_dict[name] = input_dict[name]
+        class_dicts.append(class_dict)
+
+    return class_dicts
+
+
+def plot_dict_hist(dict_data, fig_name):
+    data = []
+    for name in dict_data:
+        data.append(dict_data[name])
+    
+    data = np.concatenate(data)
+    
+    plt.hist(data, 100, histtype='step', linewidth=1.5, label=fig_name)
+    plt.legend()
+
+def extract_prototypes(embs, th=0.8):
+    embs = np.array(embs)
+    prototype = embs.mean(0)
+    dists = np.linalg.norm(embs - prototype, axis=-1)
+    th_val = np.sort(dists)[int(len(dists) * th)]
+    valid_inds = np.argwhere(dists < th_val).ravel()
+    final_prototype = embs[valid_inds].mean(0)
+    return final_prototype
+
+def refine_group_prototypes(group_embs, n_iter=2):
+    all_embs = np.concatenate(group_embs)
+
+    prototypes = [embs.mean(0) for embs in group_embs]
+    prototypes = np.array(prototypes)
+    
+    # print([group_embs[j].shape for j in range(len(group_embs))]) 
+    
+    for k in range(n_iter):
+        dists = np.linalg.norm(all_embs[..., None] - prototypes.T[None], axis=1)
+        labels = np.argmin(dists, axis=1)
+        new_embs = []
+        for l in np.unique(labels):
+            inds = np.argwhere(labels == l).ravel()
+            new_embs.append(all_embs[inds])
+
+        # print([new_embs[j].shape for j in range(len(new_embs))]) 
+    
+        prototypes = [embs.mean(0) for embs in new_embs]
+        prototypes = np.array(prototypes)
+    # print()
+    
+    
+    return prototypes
+    
+    
+def cluster_group_prototypes(group_embs, n_iter=100):
+    n_c = len(group_embs)
+    kmeans = KMeans(n_clusters=n_c, max_iter=n_iter)
+    all_embs = np.concatenate(group_embs)
+    
+    kmeans.fit(all_embs)
+
+    prototypes = kmeans.cluster_centers_
+    # print(prototypes.shape)
+    
+    return prototypes
+    
+
+
+    
+print()
+dist_utils.calc_dists_ratio(train_dict, ood_dict)
+dist_utils.calc_dists_ratio(test_dict, ood_dict)
+
+train_dict_list = get_class_dicts(train_dict)
+test_dict_list = get_class_dicts(test_dict)
+
+ood_embs = np.concatenate([ood_dict[key] for key in ood_dict.keys()])
+
+train_prototypes = []
+train_embs = []
+for data in train_dict_list:
+    all_data = []
+    for key in data.keys():
+        all_data.append(data[key])
+        
+    all_data = np.concatenate(all_data)
+    train_embs.append(all_data)
+    train_prototypes.append(all_data.mean(0))
+
+
+#train_prototypes = [train_dict[key].mean(0) for key in train_dict.keys()]
+train_prototypes = np.array(train_prototypes)
+
+print('OOD:')
+print('Prototypical:')
+#dist_utils.calc_ROC(test_dict_list[0], ood_embs, prototypes=train_dict_list[0])
+#dist_utils.calc_ROC(test_dict_list[1], ood_embs, prototypes=train_dict_list[1])
+
+network_name = 'ResNet50' if backbone == 'res50' else 'DINO-v2 (Normalized)'
+
+dist_utils.calc_ROC(test_dict, ood_embs, prototypes=train_prototypes, plot=False,
+                    exp_name='Prototypical', network_name=network_name)
+
+
+x_train = np.concatenate(train_embs)
+y_train = np.concatenate([np.zeros(len(train_embs[0])), np.ones(len(train_embs[1]))])
+
+
+test_embs = np.concatenate([test_dict[k] for k in test_dict.keys()])
+
+class DummyConfig:
+    class Postprocessor:
+        postprocessor_args = {}
+    postprocessor = Postprocessor()
+    
+    
+config = DummyConfig()
+sprod1 = SPROD1(config=config, probabilistic_score=False, normalize_features=True)
+
+train_preds, train_confs, _ = sprod1.numpy_inference(x_train, y_train)
+test_preds, test_confs, _ = sprod1.numpy_inference(test_embs)
+ood_preds, ood_confs, _ = sprod1.numpy_inference(ood_embs)
+
+print('sprod-1:')
+dist_utils.calc_ROC_with_dists(-test_confs, -ood_confs)
+
+
+dists = np.linalg.norm(x_train[..., None] - train_prototypes.T[None], axis=1)
+y_hat_train = np.argmin(dists, -1)  
+
+
+total_misc_inds = np.argwhere(y_hat_train != y_train).ravel()
+total_crr_inds = np.argwhere(y_hat_train == y_train).ravel()
+
+aug_prototypes = []
+aug_embs = []
+for l in [0, 1]:
+    class_inds = np.argwhere(y_train == l).ravel()
+    class_miss_inds = np.intersect1d(class_inds, total_misc_inds, assume_unique=True)
+    aug_prototypes.append(x_train[class_miss_inds].mean(0))
+    aug_embs.append(x_train[class_miss_inds])
+    class_crr_inds = np.intersect1d(class_inds, total_crr_inds, assume_unique=True)
+    aug_prototypes.append(x_train[class_crr_inds].mean(0))
+    aug_embs.append(x_train[class_crr_inds])
+    
+refined_prototypes = []
+
+refined_prototypes.extend(refine_group_prototypes(aug_embs[:2]))
+refined_prototypes.extend(refine_group_prototypes(aug_embs[2:]))
+
+aug_prototypes = np.array(aug_prototypes)
+print('Prototypical-GI:')
+dist_utils.calc_ROC(test_dict, ood_embs, prototypes=aug_prototypes, plot=False,
+                    exp_name='Prototypical-GI', network_name=network_name)
+
+print('after (refined):')
+dist_utils.calc_ROC(test_dict, ood_embs, prototypes=refined_prototypes, plot=False)
+
+aug_prototypes = np.array(aug_prototypes)
+aug_prototypes2 = [aug_prototypes[:2].mean(0), aug_prototypes[2:].mean(0)]
+print('Prototypical-GI-MG:')
+dist_utils.calc_ROC(test_dict, ood_embs, prototypes=aug_prototypes2, plot=False,
+                    exp_name='Prototypical-GI-MG', network_name=network_name)
+
+n_c = 2
+
+kmeans_prototypes = []
+for c in range(n_c):
+    kmeans_prototypes.extend(cluster_group_prototypes(aug_embs[n_c*c: n_c*(c+1)]))
+    
+print('Prototypical-KMEANS:')
+dist_utils.calc_ROC(test_dict, ood_embs, prototypes=kmeans_prototypes, plot=False,
+                    exp_name='Prototypical-KMEANS', network_name=network_name)
+
+
+
+
+# aug_prototypes = np.concatenate([aug_prototypes, train_prototypes])
+# print('after after:')
+# dist_utils.calc_ROC(test_dict, ood_embs, prototypes=aug_prototypes, plot=True)
+
+# inds1 = [0, 1, 4]
+# inds2 = [2, 3, 5]
+
+# aug_prototypes2 = [aug_prototypes[inds1].mean(0), aug_prototypes[inds2].mean(0)]
+# print('after after2:')
+# dist_utils.calc_ROC(test_dict, ood_embs, prototypes=aug_prototypes2, plot=True)
+
+
+
+if False:
+    n_cluster = 4
+    kmeans_protos = []
+    
+    def propose_centers(embeddings):
+        centers = []
+        for embs in embeddings:
+            for j in range(n_cluster):
+                mean = embs.mean(0)
+                std = embs.std(0)
+                if j == 0:
+                    centers.append(mean)
+                else:
+                    centers.append(np.random.normal(mean, std / 0.2))
+        return centers
+    
+    class1_protos = propose_centers(aug_embs[:2])
+    kmeans = KMeans(n_clusters=n_cluster*2, random_state=0, init=class1_protos, max_iter=15).fit(train_embs[0])
+    kmeans_protos.append(kmeans.cluster_centers_)
+    
+    class2_protos = propose_centers(aug_embs[2:])
+    kmeans = KMeans(n_clusters=n_cluster*2, random_state=0, init=class2_protos, max_iter=15).fit(train_embs[1])
+    kmeans_protos.append(kmeans.cluster_centers_)
+    
+    kmeans_protos = np.concatenate(kmeans_protos)
+    aug_prototypes = np.concatenate([aug_prototypes, kmeans_protos])
+    # print('kmeans:')
+    # dist_utils.calc_ROC(test_dict, ood_embs, prototypes=aug_prototypes)
+    
+    
