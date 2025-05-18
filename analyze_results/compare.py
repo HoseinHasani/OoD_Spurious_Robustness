00d0c4641142fd51b9015df0ffc5bf1f3039a124
Hosein <hosein.hasani@sharif.edu>
2025-05-18 20:25:46 +0330

    Update the compare code

--- analyze_results/compare.py
+++ analyze_results/compare.py
@@ -4,13 +4,30 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
+
+near_ood_dataset = ['animals_ood', 'clbood', 'spurious_imagenet', 'urbn_no_car_ood', 'placesbg']
+dataset = ['animals_metacoco', 'celeba_blond', 'spurious_imagenet', 'urbancars', 'waterbirds']
+
+hard_correlations_dict ={
+        'waterbirds': 90,
+        'celeba_blond': 0.9,
+        'urbancars': 95,
+        'animals_metacoco': 95,
+        'spurious_imagenet': 95
+        }
+
+select = 0
+
+dataset_name = dataset[select]
+corr = hard_correlations_dict[dataset_name]
+
 directory = "pickles"
 
 metric = "AUROC"
 filters = {
-    "dataset": "spurious_imagenet",
-    "ood_set": "spurious_imagenet",
-    "correlation": "r95",
+    "dataset": dataset_name,
+    "ood_set": near_ood_dataset[select],
+    "correlation": f"r{corr}",
     "flag": "default",  
 }
 
@@ -60,11 +77,11 @@ group_keys = fields_to_compare.copy()
 df_avg = df.groupby(group_keys)[metric].mean().reset_index()
 
 pivot = df_avg.pivot(index=fields_to_compare[0], columns=fields_to_compare[1], values=metric)
-
-output_csv_path = f"results_{metric.lower()}_pivot.csv"
+os.makedirs('csvs', exist_ok=True)
+output_csv_path = f"csvs/results_{dataset_name}.csv"
 pivot.round(2).to_csv(output_csv_path)
 print(f"Saved pivot table to {output_csv_path}")
 
-sns.heatmap(pivot, annot=True, fmt=".2f", cmap="viridis")
-plt.title(f"{metric} across {fields_to_compare}")
-plt.show()
+# sns.heatmap(pivot, annot=True, fmt=".2f", cmap="viridis")
+# plt.title(f"{metric} across {fields_to_compare}")
+# plt.show()
